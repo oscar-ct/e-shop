e227bb1d156798f195b998276ca5404fb3b1f289
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-17 15:07:16 -0500

    feat: create product component for prop

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Product = ( {product} ) => {
+    return (
+        <>
+            <div className="card w-96 bg-base-100 shadow-xl m-3">
+                <a href={`/product/${product._id}`}>
+                    <figure className="px-10 pt-10">
+                        <img src={product.image} alt="Shoes" className="rounded-xl" />
+                    </figure>
+                </a>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{product.name}</h2>
+                    {/*<p>{product.description}</p>*/}
+                    <div className={"w-full flex justify-start"}>
+                        <span className={"text-2xl font-bold text-slate-500"}>${product.price}</span>
+                    </div>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Product;
