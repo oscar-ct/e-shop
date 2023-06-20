8fa13d40d9e9f0f6c757b148720531f5a3f3dd93
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-19 22:38:47 -0500

    feat: implement react router, setup homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider
+} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path={"/"} element={<App/>}>
+            <Route index={true} path={"/"} element={<HomePage/>}/>
+        </Route>
+    )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
