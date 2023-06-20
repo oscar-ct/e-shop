3cb34a802904a9f67a86b5142193950b75127438
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-19 22:39:16 -0500

    feat: removed homepage and setup outlet from react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
+import {Outlet} from 'react-router-dom';
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-
 
 function App() {
   return (
       <>
           <Navbar/>
           <div className={"container m-auto"}>
-              <HomePage/>
+              <Outlet/>
           </div>
       </>
 
