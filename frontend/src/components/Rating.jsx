ca66920c7d5b9638ad4b7059e844c059f184c18f
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-20 23:51:03 -0500

    feat: created rating component, styled and formatted

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa'
+
+const Rating = ( {rating, text} ) => {
+    return (
+        <div className={"flex items-center"}>
+            <span className={"text-yellow-400"}>
+                {rating >= 1 ? <FaStar/> : rating >= 0.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
+            </span>
+            <span className={"text-yellow-400"}>
+                {rating >= 2 ? <FaStar/> : rating >= 1.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
+            </span>
+            <span className={"text-yellow-400"}>
+                {rating >= 3 ? <FaStar/> : rating >= 2.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
+            </span>
+            <span className={"text-yellow-400"}>
+                {rating >= 4 ? <FaStar/> : rating >= 3.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
+            </span>
+            <span className={"text-yellow-400"}>
+                {rating >= 5 ? <FaStar/> : rating >= 4.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
+            </span>
+            <span className={"pl-1"}>
+                {text && text}
+            </span>
+        </div>
+    );
+};
+
+export default Rating;
