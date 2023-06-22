af826a604a790de48940865995b898a817f5303e
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-21 23:22:04 -0500

    feat: run db connection, add comments

--- backend/index.js
+++ backend/index.js
@@ -2,28 +2,33 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
-
-
+import connectDB from './configDatabaseConnection/db.js'
 const port = process.env.PORT || 8080;
 
+
+
+
+// 1). Establish database connection
+connectDB();
+// 2). Initialize backend framework
 const app = express();
 
 
 app.get('/', function (request, response) {
     response.send("API is running!!!");
 });
-
 app.get('/api/products', function (req, res) {
     res.json(products);
 });
-
-
 app.get('/api/products/:id', function (req, res) {
     const product = products.find(function (product) {
         return product._id === req.params.id;
     });
     res.json(product);
 })
+
+
+
 
 app.listen(port, function () {
     console.log("Server is active on port " + port);
