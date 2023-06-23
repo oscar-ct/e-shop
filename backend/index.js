b281de38abad36448194d257b73d557f03978980
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-23 17:37:04 -0500

    feat: setup app error middlwware

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,28 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './configDatabaseConnection/db.js'
+import productRoutes from "./routes/productRoutes.js";
+import {notFoundError, errorHandler} from "./middleware/errorMiddleware.js";
+
 const port = process.env.PORT || 8080;
 
 
 
 
-// 1). Establish database connection
+// 1). Establish database connection to MongoDB from Mongoose
 connectDB();
-// 2). Initialize backend framework
+// 2). Initialize Express framework
 const app = express();
 
 
 app.get('/', function (request, response) {
     response.send("API is running!!!");
 });
-app.get('/api/products', function (req, res) {
-    res.json(products);
-});
-app.get('/api/products/:id', function (req, res) {
-    const product = products.find(function (product) {
-        return product._id === req.params.id;
-    });
-    res.json(product);
-})
 
-
-
+app.use('/api/products', productRoutes);
+app.use(notFoundError);
+app.use(errorHandler);
 
 app.listen(port, function () {
     console.log("Server is active on port " + port);
