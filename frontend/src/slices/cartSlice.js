ab1bd4d83589278eec056bf974541674ce705b75
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-28 17:24:00 -0500

    feat: add comments to cart slice actions

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -11,24 +11,31 @@ const cartSlice = createSlice({
    initialState,
    reducers: {
       addToCart: function (state, action) {
-         const item = action.payload;
+         const itemUserIsAddingToCart = action.payload;
+
+         // each item has unique id, if the item the user is trying to add to cart already exist in the cart, this will return true and an object of the existing item.
          const existItem = state.cartItems.find(function (cartItem) {
-            return item._id === cartItem._id;
-         })
+            return itemUserIsAddingToCart._id === cartItem._id;
+         });
+
+         // if true/exists, we loop through and update or add the existing item in the cart
          if (existItem) {
             state.cartItems = state.cartItems.map(function (cartItem) {
+               // we locate the item with matching ids and update the old item in the cart with the new item quantity
                if (cartItem._id === existItem._id) {
-                  return item;
+                  return itemUserIsAddingToCart;
+               // we return the items that the user has stored in their cart without any changes
                } else {
                   return cartItem;
                }
-            })
+            });
+            // we add the item the user is adding to cart because the item did not exist in the cart
          } else {
-            state.cartItems = [...state.cartItems, item];
+            state.cartItems = [...state.cartItems, itemUserIsAddingToCart];
          }
 
 
-
+         // set sum of items price
          state.itemsPrice = state.cartItems.reduce(function (acc, item) {
             return acc + item.price * item.quantity;
          }, 0);
