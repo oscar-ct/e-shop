15229d1e5e49b14c8712a3f0451aac216fb7dbf4
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-28 23:35:57 -0500

    feat: created route for new cart page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import './index.css';
 import App from './App';
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 
 
 const router = createBrowserRouter(
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
         <Route path={"/"} element={<App/>}>
             <Route index={true} path={"/"} element={<HomePage/>}/>
             <Route path={"/product/:id"} element={<ProductPage/>}/>
+            <Route path={"/cart"} element={<CartPage/>}/>
         </Route>
     )
 )
