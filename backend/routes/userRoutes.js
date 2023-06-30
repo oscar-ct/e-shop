79307011684ca8901dc39d79c5c27d2ea4a24712
Oscar Castro <oscar.a.castro818@gmail.com>
2023-06-30 00:13:35 -0500

    feat: add custom middleware for protect jwt and admin routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,6 +10,7 @@ import {
     updateUsers,
     deleteUsers,
 } from "../controllers/userController.js";
+import {protect, admin} from "../middleware/authMiddleware.js";
 
 
 // *****  Router connects to index.js, cleans up code  *****
@@ -17,17 +18,20 @@ const router = express.Router();
 
 router.post('/',registerUser);
 router.post('/logout', logoutUser);
-router.get('/login',authUser);
-router.get('/profile',getUserData);
-router.put('/profile', updateUserData);
+router.get('/login', authUser);
+
+// Using custom middleware PROTECT
+router.get('/profile', protect, getUserData);
+router.put('/profile', protect, updateUserData);
 
 
 
 // ADMIN ACCESS ONLY
-router.get('/',getUsers);
-router.get('/:id',getUserById);
-router.put('/:id',updateUsers);
-router.delete('/:id', deleteUsers);
+// Using custom middleware PROTECT and ADMIN
+router.get('/',protect, admin, getUsers);
+router.get('/:id', protect, admin, getUserById);
+router.put('/:id', protect, admin, updateUsers);
+router.delete('/:id', protect, admin, deleteUsers);
 
 
 export default router;
