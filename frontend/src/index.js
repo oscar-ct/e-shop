354d0b497d2f97c62a5ef5e72feea9b825e28a5a
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-01 13:23:06 -0500

    feat: implement private route, left comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,15 +14,27 @@ import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ShippingPage from "./pages/ShippingPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path={"/"} element={<App/>}>
+
+            {/*Public Routes*/}
             <Route index={true} path={"/"} element={<HomePage/>}/>
             <Route path={"/product/:id"} element={<ProductPage/>}/>
             <Route path={"/cart"} element={<CartPage/>}/>
             <Route path={"/login"} element={<LoginPage/>}/>
+            <Route path={"/register"} element={<RegisterPage/>}/>
+
+            {/*Private Routes*/}
+            <Route path={""} element={<PrivateRoute/>}>
+                <Route path={"/shipping"} element={<ShippingPage/>}/>
+            </Route>
+
         </Route>
     )
 )
