09c9b977477538ee031089e65095c3f871dc928f
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-01 13:22:31 -0500

    feat: create private route using global auth state and outlet

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Outlet, Navigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+
+const PrivateRoute = () => {
+
+    const userData = useSelector(function (state) {
+        return state.auth;
+    });
+
+    return (
+        userData ? (
+            <Outlet/>
+            ) : (
+            <Navigate to={"/login"} replace/>
+        )
+    );
+};
+
+export default PrivateRoute;
