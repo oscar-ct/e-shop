2d1ebf257b45951d3acf43f97a84e4dd4903898b
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-01 20:53:48 -0500

    feat: create checkout steps for checkout process

---
+++ frontend/src/components/CheckoutSteps.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const CheckoutSteps = ({ step1, step2 }) => {
+    return (
+        <div className={"flex justify-center mt-5"}>
+
+            {
+                step1 ? (
+                    <div className={"steps"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
+                        <button className="step cursor-not-allowed">Payment</button>
+                        <button className="step cursor-not-allowed">Place Order</button>
+                    </div>
+                ) : step2 ?  (
+                    <div className={"steps"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
+                        <Link to={"/payment"} className="step step-primary">Payment</Link>
+                        <button className="step cursor-not-allowed">Place Order</button>
+                    </div>
+                ) : (
+                    <div className={"steps"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
+                        <Link to={"/payment"} className="step step-primary">Payment</Link>
+                        <Link to={"/placeOrder"} className="step step-primary">Place Order</Link>
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default CheckoutSteps;
