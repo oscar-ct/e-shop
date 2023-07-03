13f2f8fd5d052102dca51a8f2d8e36c2ab037ce6
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-02 21:49:16 -0500

    feat: defined 3 order controller endpoints with correct data responses

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,14 +4,66 @@ import asyncHandler from "../middleware/asyncHandler.js";
 
 
 const createOrder = asyncHandler(async (req, res) => {
-    return res.send("create order");
+    const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error("No order items found");
+    } else {
+        const newOrder = new Order({
+            user: req.user._id,
+            orderItems: orderItems.map(function (item){
+                return {
+                    ...item,
+                    product: item._id,
+                    _id: undefined,
+                }
+            }),
+            shippingAddress: shippingAddress,
+            paymentMethod: paymentMethod,
+            itemsPrice: itemsPrice,
+            taxPrice: taxPrice,
+            shippingPrice: shippingPrice,
+            totalPrice: totalPrice,
+        });
+
+        const createdOrder = await newOrder.save();
+        res.status(201);
+        return res.json(createdOrder);
+    }
 });
+
+
 const getUserOrders = asyncHandler(async (req, res) => {
-    return res.send("get user orders");
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200);
+    return res.json(orders);
 });
+
+
+const getUserOrderById = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    if (userId) {
+        const order = await Order.findById(req.params.id);
+        if (userId === order.user._id) {
+            res.status(200);
+            return res.json(order);
+        } else if (userId !== order.user._id) {
+            res.status(404);
+            throw new Error("You do not have access to this order");
+        } else {
+            res.status(404);
+            throw new Error("Order not found");
+        }
+    }
+});
+
+
 const updateOrderToPaid = asyncHandler(async (req, res) => {
     return res.send("update order to paid");
 });
+
+
+
 
 
 // ADMIN ACCESS ONLY
@@ -32,6 +84,7 @@ const getAllOrders = asyncHandler(async (req, res) => {
 export {
     createOrder,
     getUserOrders,
+    getUserOrderById,
     getOrderById,
     updateOrderToPaid,
     updateOrderShipmentStatus,
