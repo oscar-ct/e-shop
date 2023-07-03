036923624ebccfa51cbb8d9c84c99257351740ad
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-02 22:49:21 -0500

    feat: create new orders slice with 3 reducer actions

---
+++ frontend/src/slices/ordersApiSlice.js
@@ -0,0 +1,44 @@
+import {ORDERS_URL} from "../variables";
+import {apiSlice} from "./apiSlice";
+
+
+export const ordersApiSlice = apiSlice.injectEndpoints({
+    endpoints: function (build) {
+        return {
+            createOrder: build.mutation({
+                query: function (data) {
+                    return {
+                        url: ORDERS_URL,
+                        method: "POST",
+                        body: {...data},
+                    }
+                },
+                // 5 seconds
+                keepUnusedDataFor: 5
+            }),
+            getMyOrders: build.query({
+                    query: function (id) {
+                        return {
+                            url: `${ORDERS_URL}/myorders`
+                        }
+                    },
+                    // 5 seconds
+                    keepUnusedDataFor: 5
+                }
+            ),
+            getMyOrderById: build.query({
+                    query: function (id) {
+                        return {
+                            url: `${ORDERS_URL}/myorders/${id}`
+                        }
+                    },
+                    // 5 seconds
+                    keepUnusedDataFor: 5
+                }
+            ),
+
+        };
+    }
+});
+
+export const { useCreateOrderMutation, useGetProductDetailsQuery } = productsApiSlice;
