c39cbf00d11e49af0123b92cbb4cd1169540e840
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-03 18:42:30 -0500

    feat: updated checkout steps using conditionals based on state

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -1,30 +1,60 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 const CheckoutSteps = ({ step1, step2 }) => {
+
+    const {cartItems,shippingAddress, paymentMethod} = useSelector(function (state) {
+       return state.cart;
+    });
+
     return (
         <div className={"flex justify-center mt-5"}>
             {
                 step1 ? (
                     <div className={"steps"}>
-                        <Link to={"/cart"} className="step step-primary">Cart</Link>
-                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
-                        <button className="step cursor-not-allowed">Payment</button>
-                        <button className="step cursor-not-allowed">Place Order</button>
+                        <Link to={"/cart"} className="step step-primary mx-2">Cart</Link>
+                        {
+                            cartItems.length !== 0 ? (
+                                <Link to={"/shipping"} className="step step-primary mx-2">Shipping</Link>
+                            ) : (
+                                <button className="step step-primary mx-2">Shipping</button>
+                            )
+                        }
+                        {
+                            Object.keys(shippingAddress).length !== 0 ? (
+                                <Link to={"/payment"} className="step mx-2">Payment</Link>
+                            ) : (
+                                <button className="step mx-2">Payment</button>
+                            )
+                        }
+                        {
+                            paymentMethod ? (
+                                <Link to={"/checkout"} className="step mx-2">Checkout</Link>
+                            ) : (
+                                <button className="step mx-2">Checkout</button>
+                            )
+                        }
                     </div>
                 ) : step2 ?  (
                     <div className={"steps"}>
-                        <Link to={"/cart"} className="step step-primary">Cart</Link>
-                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
-                        <Link to={"/payment"} className="step step-primary">Payment</Link>
-                        <button className="step cursor-not-allowed">Place Order</button>
+                        <Link to={"/cart"} className="step step-primary mx-2">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary mx-2">Shipping</Link>
+                        <Link to={"/payment"} className="step step-primary mx-2">Payment</Link>
+                        {
+                            paymentMethod ? (
+                                <Link to={"/checkout"} className="step mx-2">Checkout</Link>
+                            ) : (
+                                <button className="step mx-2">Checkout</button>
+                            )
+                        }
                     </div>
                 ) : (
                     <div className={"steps"}>
-                        <Link to={"/cart"} className="step step-primary">Cart</Link>
-                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
-                        <Link to={"/payment"} className="step step-primary">Payment</Link>
-                        <Link to={"/placeorder"} className="step step-primary">Place Order</Link>
+                        <Link to={"/cart"} className="step step-primary mx-2">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary mx-2">Shipping</Link>
+                        <Link to={"/payment"} className="step step-primary mx-2">Payment</Link>
+                        <Link to={"/checkout"} className="step step-primary mx-2">Checkout</Link>
                     </div>
                 )
             }
