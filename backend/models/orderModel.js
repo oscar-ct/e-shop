f1dcaf6f942660e3099a20e56689a499a468da0c
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-05 22:08:03 -0500

    feat: update order model to display user name and email

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,10 +2,20 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-    },
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: "User",
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+            },
+        },
     orderItems: [
         {
             name: {
