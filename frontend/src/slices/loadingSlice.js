aa435ec9ba54d39e841fdeca1f23e75870cae527
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-06 23:39:09 -0500

    feat: create new slice to trigger loading screen with opacity

---
+++ frontend/src/slices/loadingSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    loading: false
+};
+
+const loadingSlice = createSlice({
+    name: "loading",
+    initialState,
+    reducers: {
+        setLoading: function (state, action) {
+            state.loading = action.payload;
+        },
+    },
+
+});
+
+export const {setLoading} = loadingSlice.actions;
+
+export default loadingSlice.reducer;
