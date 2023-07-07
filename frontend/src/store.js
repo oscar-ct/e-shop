1925d7a611bb64e1eb8ce6826d00b6bb03ecce55
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-06 23:41:47 -0500

    feat: add loading state to redux provider

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import {apiSlice} from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
 import authSliceReducer from "./slices/authSlice";
+import loadingSliceReducer from "./slices/loadingSlice";
 
 const store = configureStore({
    reducer: {
       [apiSlice.reducerPath]: apiSlice.reducer,
       cart: cartSliceReducer,
       auth: authSliceReducer,
+      loading: loadingSliceReducer,
 
    },
    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
