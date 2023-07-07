053e10e6b0d6137a360c152b3fd928ce77cbe929
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-06 23:38:11 -0500

    feat: brought in state to load custom spinner with opacity to main app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import {Outlet} from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import {useSelector} from "react-redux";
+
 
 function App() {
+
+    const loading = useSelector(function (state) {
+        return state.loading;
+    });
   return (
       <>
-          <Navbar/>
-          <div className={"min-h-[calc(100vh-80px)]"}>
-              <div className={"container md:px-5 xl:px-10 m-auto min-h-[calc(100vh-80px)]"}>
-                  <Outlet/>
+          {
+              loading.loading && (
+                  <div className={"absolute z-10 w-full h-screen flex justify-center"}>
+                      <span className="loading loading-bars loading-lg"/>
+                  </div>
+              )
+          }
+          <div className={loading.loading ? "opacity-30" : ""}>
+              <Navbar/>
+              <div className={"min-h-[calc(100vh-80px)]"}>
+                  <div className={"container md:px-5 xl:px-10 m-auto min-h-[calc(100vh-80px)]"}>
+                      <Outlet/>
+                  </div>
               </div>
           </div>
       </>
