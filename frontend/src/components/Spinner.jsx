e2b12308b60d6d0fe33359b62196e30ad2a4dd8c
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-07 19:37:11 -0500

    feat: update spinner styling

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div className={"w-full h-screen flex justify-center"}>
+        <div className={"w-full min-h-[calc(100vh-80px)] flex justify-center"}>
             <span className="loading loading-bars loading-lg"/>
         </div>
     );
