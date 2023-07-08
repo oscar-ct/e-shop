201f0b4e4432a0fabc6ee025650353a1c7e2c3ce
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-07 19:32:14 -0500

    feat: add first step for cart page, adjusted size

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 
-const CheckoutSteps = ({ step1, step2 }) => {
+const CheckoutSteps = ({ step1, step2, step3 }) => {
 
     const {cartItems,shippingAddress, paymentMethod} = useSelector(function (state) {
        return state.cart;
@@ -12,49 +12,74 @@ const CheckoutSteps = ({ step1, step2 }) => {
         <div className={"flex justify-center py-5"}>
             {
                 step1 ? (
-                    <div className={"steps"}>
-                        <Link to={"/cart"} className="step step-primary mx-2">Cart</Link>
+                    <div className={"steps w-full"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
                         {
                             cartItems.length !== 0 ? (
-                                <Link to={"/shipping"} className="step step-primary mx-2">Shipping</Link>
+                                <Link to={"/shipping"} className="step">Shipping</Link>
                             ) : (
-                                <button className="step step-primary mx-2">Shipping</button>
+                                <button className="step">Shipping</button>
                             )
                         }
                         {
                             Object.keys(shippingAddress).length !== 0 ? (
-                                <Link to={"/payment"} className="step mx-2">Payment</Link>
+                                <Link to={"/payment"} className="step">Payment</Link>
+                            ) : (
+                                <button className="step">Payment</button>
+                            )
+                        }
+                        {
+                            paymentMethod ? (
+                                <Link to={"/checkout"} className="step">Checkout</Link>
+                            ) : (
+                                <button className="step">Checkout</button>
+                            )
+                        }
+                    </div>
+                ) : step2 ? (
+                    <div className={"steps w-full"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
+                        {
+                            cartItems.length !== 0 ? (
+                                <Link to={"/shipping"} className="step step-primary">Shipping</Link>
+                            ) : (
+                                <button className="step step-primary">Shipping</button>
+                            )
+                        }
+                        {
+                            Object.keys(shippingAddress).length !== 0 ? (
+                                <Link to={"/payment"} className="step">Payment</Link>
                             ) : (
                                 <button className="step mx-2">Payment</button>
                             )
                         }
                         {
                             paymentMethod ? (
-                                <Link to={"/checkout"} className="step mx-2">Checkout</Link>
+                                <Link to={"/checkout"} className="step">Checkout</Link>
                             ) : (
-                                <button className="step mx-2">Checkout</button>
+                                <button className="step">Checkout</button>
                             )
                         }
                     </div>
-                ) : step2 ?  (
-                    <div className={"steps"}>
-                        <Link to={"/cart"} className="step step-primary mx-2">Cart</Link>
-                        <Link to={"/shipping"} className="step step-primary mx-2">Shipping</Link>
-                        <Link to={"/payment"} className="step step-primary mx-2">Payment</Link>
+                ) : step3 ?  (
+                    <div className={"steps w-full"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
+                        <Link to={"/payment"} className="step step-primary">Payment</Link>
                         {
                             paymentMethod ? (
-                                <Link to={"/checkout"} className="step mx-2">Checkout</Link>
+                                <Link to={"/checkout"} className="step">Checkout</Link>
                             ) : (
-                                <button className="step mx-2">Checkout</button>
+                                <button className="step">Checkout</button>
                             )
                         }
                     </div>
                 ) : (
-                    <div className={"steps"}>
-                        <Link to={"/cart"} className="step step-primary mx-2">Cart</Link>
-                        <Link to={"/shipping"} className="step step-primary mx-2">Shipping</Link>
-                        <Link to={"/payment"} className="step step-primary mx-2">Payment</Link>
-                        <Link to={"/checkout"} className="step step-primary mx-2">Checkout</Link>
+                    <div className={"steps w-full"}>
+                        <Link to={"/cart"} className="step step-primary">Cart</Link>
+                        <Link to={"/shipping"} className="step step-primary">Shipping</Link>
+                        <Link to={"/payment"} className="step step-primary">Payment</Link>
+                        <Link to={"/checkout"} className="step step-primary">Checkout</Link>
                     </div>
                 )
             }
