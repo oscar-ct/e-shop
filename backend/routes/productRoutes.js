8df705430fe223f836bcb44bdfb18edf8f06a8fa
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-11 15:39:14 -0500

    feat: add new route for create product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {getAllProducts, getProductById} from "../controllers/productController.js";
-
+import {getAllProducts, getProductById, createProduct} from "../controllers/productController.js";
+import {protect, admin} from "../middleware/authMiddleware.js";
 
 // *****  Router connects to index.js, cleans up code  *****
 const router = express.Router();
@@ -8,6 +8,10 @@ const router = express.Router();
 // router.route('/').get(getAllProducts);
 router.get('/',getAllProducts);
 router.get('/:id', getProductById);
+
+
+// ADMIN ACCESS ONLY
+router.post("/", protect, admin, createProduct);
 
 
 export default router;
