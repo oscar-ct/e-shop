1838d344b6cd96e7c89f5a4602314df3e9192b42
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-13 19:23:19 -0500

    feat: add images array to product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -74,7 +74,17 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: true,
         default: 0,
-    }
+    },
+    images: [
+        {
+            url: {
+                type: String
+            },
+            handle: {
+                type: String
+            }
+        }
+    ]
 
 }, {
     timestamps: true
