dd3a05de33c2bd95f05f4ca6e71667be289202c5
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-15 17:49:34 -0500

    new feature: admin tabs to control for quick access to admin tools

---
+++ frontend/src/components/AdminTabs.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link, useLocation} from "react-router-dom";
+
+const AdminTabs = () => {
+
+    const location = useLocation();
+
+    return (
+        <div className={"p-5 flex justify-center"}>
+            <div className="tabs tabs-boxed bg-neutral">
+                <Link
+                    to={"/admin/orders"}
+                    className={`tab text-neutral-content ${location.pathname === "/admin/orders" ? "tab-active" : "text-neutral-content"}`}>
+                    Orders
+                </Link>
+                <Link
+                    to={"/admin/users"}
+                    className={`tab ${location.pathname === "/admin/users" ? "bg-info text-neutral" : "text-neutral-content"}`}>
+                    Users
+                </Link>
+                <Link
+                    to={"/admin/products"}
+                    className={`tab ${location.pathname === "/admin/products" ? "bg-info text-neutral" : "text-neutral-content"}`}>
+                    Products
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default AdminTabs;
