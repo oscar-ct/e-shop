4cd754daa165977edcee7fa691d8f515e07e23ec
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-17 23:26:50 -0500

    inlcude searchTerm to paginate component

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,13 +1,13 @@
 import {Link} from "react-router-dom";
 import React from 'react';
 
-const Paginate = ({pages, page, isAdmin = false}) => {
+const Paginate = ({pages, page, isAdmin = false, searchTerm = ""}) => {
 
     return (
        pages > 1 && (
                [...Array(pages).keys()].map(function (pg, index) {
                     return (
-                        <Link className={`join-item btn ${pg + 1 === page && "bg-primary text-base-100 hover:text-neutral"}`} key={index} to={!isAdmin ? `/page/${pg + 1}` : `/admin/products/page/${pg + 1}`}>
+                        <Link className={`join-item btn ${pg + 1 === page && "bg-primary text-base-100 hover:text-neutral"}`} key={index} to={!isAdmin ? searchTerm ? `/search/${searchTerm}/page/${pg+1}` : `/page/${pg + 1}` : `/admin/products/page/${pg + 1}`}>
                            {pg + 1}
                         </Link>
                     )
