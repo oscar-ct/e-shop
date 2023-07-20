525efe4a55fc4a5f9a1b4fcc6eef352eec0c1331
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-19 19:40:40 -0500

    feat: add carousel to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -62,22 +62,22 @@ const HomePage = () => {
                                             slidesPerView={1}
                                         >
                                             <SwiperSlide>
-                                                <div className={"h-[32em] rounded-br-xl rounded-bl-xl"} style={{background: "url(https://plus.unsplash.com/premium_photo-1681488484866-af8f282d59ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1557&q=80)", backgroundPosition: "bottom", backgroundSize: "cover"}}>
-                                                    <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
-                                                        <span style={{fontFamily: 'Ubuntu'}} className={"text-6xl"}>Welcome to e-shop!</span>
+                                                <div className={"h-[30em] rounded-br-xl rounded-bl-xl"} style={{background: "url(https://plus.unsplash.com/premium_photo-1681488484866-af8f282d59ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1557&q=80)", backgroundPosition: "bottom", backgroundSize: "cover"}}>
+                                                    <div className={"absolute w-full text-center top-[45%]"}>
+                                                        <span style={{fontFamily: 'Ubuntu'}} className={"text-6xl"}><span className={"font-bold text-base-300"}>Welcome</span> to e-shop!</span>
                                                     </div>
                                                 </div>
                                             </SwiperSlide>
                                             <SwiperSlide>
-                                                <div className={"h-[32em] rounded-br-xl rounded-bl-xl"} style={{background: "url( https://images.unsplash.com/photo-1641350625112-3b1d73c71418?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80)", backgroundPosition: "center", backgroundSize: "cover"}}>
-                                                    <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+                                                <div className={"h-[30em] rounded-br-xl rounded-bl-xl"} style={{background: "url( https://images.unsplash.com/photo-1641350625112-3b1d73c71418?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80)", backgroundPosition: "center", backgroundSize: "cover"}}>
+                                                    <div className={"absolute w-full text-center top-[40%]"}>
                                                         <span style={{fontFamily: 'Ubuntu'}} className={"text-5xl text-white font-bold"}>Shop safe and secure with PayPal</span>
                                                     </div>
                                                 </div>
                                             </SwiperSlide>
                                             <SwiperSlide>
-                                                <div className={"h-[32em] rounded-br-xl rounded-bl-xl"} style={{background: "url(https://images.unsplash.com/photo-1524678714210-9917a6c619c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80)", backgroundPosition: "top", backgroundSize: "cover"}}>
-                                                    <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+                                                <div className={"h-[30em] rounded-br-xl rounded-bl-xl"} style={{background: "url(https://images.unsplash.com/photo-1524678714210-9917a6c619c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80)", backgroundPosition: "top", backgroundSize: "cover"}}>
+                                                    <div className={"absolute w-full text-center top-[40%]"}>
                                                         <span style={{fontFamily: 'Ubuntu'}} className={"text-6xl"}>Fast 3-day shipping! </span>
                                                     </div>
                                                 </div>
