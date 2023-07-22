28aee37adb298885e23c22fd4bd422fc31d81e97
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-21 21:18:56 -0500

    feat: update img src, clean up code, minor style update

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,51 +1,36 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
 import {FaTrash} from "react-icons/fa";
 import {addToCart, removeFromCart} from "../slices/cartSlice";
 import {useDispatch} from "react-redux";
-import {setLoading} from "../slices/loadingSlice";
 import {formatPrice} from "../utils/formatPriceUtilis";
 
 const CartItem = ( {item} ) => {
 
-    const [quantity, setQuantity] = useState(null);
-    const [itemUpdated, setItemUpdated] = useState(false);
-
     const dispatch = useDispatch();
-
-
     const addToCartHandler = async (item, quantity) => {
-        dispatch(setLoading(true));
-        // setItemUpdated(true);
         dispatch(addToCart({
             ...item, quantity
         }));
-        // setTimeout(function () {
-        //     setItemUpdated(false);
-        // }, 1000)
-        dispatch(setLoading(false));
     };
     const removeFromCartHandler = async (id) => {
         dispatch(removeFromCart(id));
     };
 
-
     return (
         <>
-            <div className={"my-5 border-b-2 border-grey-500"}/>
             <div className={"flex w-full"}>
-
                 <div className={"w-3/12"}>
                     <Link to={`/product/${item._id}`}>
-                        <img className={"lg:w-56 rounded-xl"} src={item.image} alt={"cartItem"}/>
+                        <img className={"lg:w-56 rounded-xl"} src={item.images[0].url} alt={"cartItem"}/>
                     </Link>
                 </div>
 
                 <div className={"w-7/12"}>
                     <div className={"flex flex-col px-5"}>
-                        <div className={"lg:text-xl font-bold text-neutral"}>
+                        <Link to={`/product/${item._id}`} className={"lg:text-lg font-bold text-neutral hover:link hover:link-primary"}>
                             {item.name}
-                        </div>
+                        </Link>
 
                         <div className={"flex flex-col lg:pt-3"}>
                             <div className={"flex flex-col text-xs sm:text-sm"}>
@@ -80,7 +65,7 @@ const CartItem = ( {item} ) => {
                                         List Price:
                                     </span>
                                     <span className={"ml-1"}>
-                                        ${item.price}/ea.
+                                        ${item.price}/ea
                                     </span>
                                 </div>
                             </div>
@@ -89,57 +74,29 @@ const CartItem = ( {item} ) => {
                 </div>
 
                 <div className={"w-2/12 flex flex-col items-end justify-between"}>
-                    {
-                        formatPrice(item.price * item.quantity, "text-xl")
-                    }
-                    {/*<span className={"lg:text-xl"}>*/}
-                    {/*    ${(item.price * item.quantity).toFixed(2)}*/}
-                    {/*</span>*/}
-
-                    {/*<div className={"bg-green-500 flex"}>*/}
-                    {/*    <div className={"flex bg-red-500"}>*/}
-                            <div className={"py-2 flex items-center"}>
-                                <label className="block mr-2 text-sm font-medium text-gray-900 dark:text-white">
-                                    Qty:
-                                </label>
-                                <select
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        value={item.quantity}
-                                        onChange={(e) => addToCartHandler(item, Number(e.target.value))}
-                                >
-                                    {
-                                        [...Array(item.countInStock).keys()].map(function (x) {
-                                            return (
-                                                <option key={x+1} value={x+1}>
-                                                    {x+1}
-                                                </option>
-                                            )
-                                        })
-                                    }
-                                </select>
-                            </div>
-                            {/*<div className={"w-20 flex justify-center items-center"}>*/}
-                            {/*    {*/}
-                            {/*        !itemUpdated ? (*/}
-                            {/*            <button*/}
-                            {/*                // onClick={addToCartHandler}*/}
-                            {/*                className={"btn btn-xs"}>*/}
-                            {/*                Update*/}
-                            {/*            </button>*/}
-                            {/*        ) : (*/}
-                            {/*            <button*/}
-                            {/*                className={"btn btn-success btn-xs"}>*/}
-                            {/*                Updated!*/}
-                            {/*            </button>*/}
-                            {/*        )*/}
-                            {/*    }*/}
-
-                            {/*</div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-
-
-
+                {
+                    formatPrice(item.price * item.quantity, "text-xl")
+                }
+                    <div className={"py-2 flex items-center"}>
+                        <label className="block mr-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Qty:
+                        </label>
+                        <select
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value={item.quantity}
+                                onChange={(e) => addToCartHandler(item, Number(e.target.value))}
+                        >
+                            {
+                                [...Array(item.countInStock).keys()].map(function (x) {
+                                    return (
+                                        <option key={x+1} value={x+1}>
+                                            {x+1}
+                                        </option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
                     <div>
                         <button
                             onClick={() => removeFromCartHandler(item._id)}
@@ -148,15 +105,10 @@ const CartItem = ( {item} ) => {
                             <FaTrash className={"text-sm text-red-500"}/>
                         </button>
                     </div>
-
-
-
-
                 </div>
-
             </div>
+            <div className={"my-5 border-b-[1px] border-gray-300"}/>
         </>
-
     );
 };
 
