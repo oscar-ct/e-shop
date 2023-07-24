faf78a8bb935888cb7a3785d69170d3c8a7c1669
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-23 23:01:15 -0500

    feat: set hard coded img src when null, minor style update

--- frontend/src/components/ProfileOrderItemProduct.jsx
+++ frontend/src/components/ProfileOrderItemProduct.jsx
@@ -5,22 +5,27 @@ const ProfileOrderItemProduct = ({product, index, orderSize}) => {
 
     return (
         <>
-            <div className={"pt-5 px-5"}>
+            <div className={"py-5 px-5"}>
                 <div className={"flex flex-col lg:flex-row"}>
-                    <div className={"w-full lg:w-8/12"}>
+                    <div className={"w-full"}>
                         <div className={"flex bg-base-100 pb-3"}>
                             <div className="avatar pr-5">
                                 <div className="rounded-xl w-24 h-24">
-                                    <img src={product.image}  alt={"product"}/>
+                                    <img src={product.images.length !== 0 ? product.images[0].url : "/images/sample.jpg"}  alt={"product"}/>
                                 </div>
                             </div>
                             <div className={"flex flex-col"}>
                                 <span className={"font-bold"}>
                                     {product.name}
                                 </span>
-                                <span className={"text-xs"}>
-                                    ${product.price} / ea.
-                                </span>
+                                <div className={"flex"}>
+                                    <span className={"text-xs"}>
+                                        ${product.price}/ea
+                                    </span>
+                                    <span className={"pl-10 text-xs"}>
+                                        Qty: {product.quantity}
+                                    </span>
+                                </div>
                                 <div className={"pt-3"}>
                                     <button className={"btn btn-warning w-36"}>
                                         Buy it again
@@ -29,28 +34,13 @@ const ProfileOrderItemProduct = ({product, index, orderSize}) => {
                             </div>
                         </div>
                     </div>
-                    <div className={"w-full lg:w-4/12"}>
-                        <div className={"flex flex-col items-center bg-base-500 p-5"}>
-                            <div className={"p-2 w-full"}>
-                                <button className={"btn text-xs btn-sm w-full"}>
-                                    Return or replace
-                                </button>
-                            </div>
-                            <div className={"p-2 w-full "}>
-                                <button className={"btn text-xs btn-sm w-full"}>
-                                    Write a product review
-                                </button>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
             {
                 index+1 !== orderSize && (
-                    <div className={"border-b-2 border-grey-500 mx-5"}/>
+                    <div className={"border-b-[1px] border-gray-300 mx-5"}/>
                 )
             }
-
         </>
     );
 };
