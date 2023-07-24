1ef6248c7e087fb93ed92b101d3eb371d0da6c5e
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-23 22:58:31 -0500

    feat: update error msg

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -158,7 +158,7 @@ const createProductReview = asyncHandler(async (req, res) => {
         });
         if (alreadyReviewed) {
             res.status(400);
-            throw new Error("Product already reviewed");
+            throw new Error("You already reviewed this product!");
         }
         const review = {
             name: req.user.name,
