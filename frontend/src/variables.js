61881d95192a31053940aaae5228a73366298eab
Oscar Castro <oscar.a.castro818@gmail.com>
2023-07-27 18:40:16 -0500

    add variables for swiper slider homepage images

--- frontend/src/variables.js
+++ frontend/src/variables.js
@@ -5,3 +5,9 @@ export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
 export const FILESTACK_URL = "/api/filestack";
+
+
+// home page images here:
+export const HOME_IMAGE_1 = "https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80";
+export const HOME_IMAGE_2 = "https://images.unsplash.com/photo-1641350625112-3b1d73c71418?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80";
+export const HOME_IMAGE_3 = "https://images.unsplash.com/photo-1575203091586-611fe505bb0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
