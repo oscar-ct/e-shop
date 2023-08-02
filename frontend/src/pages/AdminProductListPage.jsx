5189c7ff68416feec7afd3479f9ce060b0ed3d02
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-02 13:12:58 -0500

    feat: add confirm modal to handler product deletion, minor style updates

--- frontend/src/pages/AdminProductListPage.jsx
+++ frontend/src/pages/AdminProductListPage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Spinner from "../components/Spinner";
-import {FaCheckCircle, FaEdit, FaImages, FaMinusCircle, FaTimes, FaPlus, FaTrash} from "react-icons/fa";
+import {FaCheckCircle, FaEdit, FaImages, FaMinusCircle, FaTimes, FaPlus} from "react-icons/fa";
 import {Link, useNavigate} from "react-router-dom";
 import {
     useGetProductsQuery,
@@ -16,6 +16,8 @@ import * as filestack from "filestack-js";
 import AdminTabs from "../components/AdminTabs";
 import {useParams} from "react-router-dom";
 import Paginate from "../components/Paginate";
+import alertModal from "../components/AlertModal";
+import AlertModal from "../components/AlertModal";
 
 
 const AdminProductListPage = () => {
@@ -92,7 +94,7 @@ const AdminProductListPage = () => {
         // }
     };
 
-    const openImagesHandler = (id) => {
+    const openImagesModal = (id) => {
         setModalIsOpen(true);
         setProductId(id);
         window.images_modal.showModal();
@@ -212,45 +214,41 @@ const AdminProductListPage = () => {
         return message;
     };
     const submitDeleteProduct = async () => {
-        const confirm = window.confirm("Are you sure you want to delete this product?");
-        if (confirm) {
-            dispatch(setLoading(true));
-            try {
-                /// deletes product from database
-                await deleteProduct(productId);
+        dispatch(setLoading(true));
+        try {
+            /// deletes product from database
+            await deleteProduct(productId);
 
-                /// deletes product images from the cloud
-                const deletedProduct = localData.filter(function (obj) {
-                    return obj._id === productId;
-                });
-                if (deletedProduct[0].images.length !== 0) {
-                    await Promise.all(deletedProduct[0].images.map(async function (image) {
-                        if (image.handle !== "sampleImage") {
-                            // console.log("attempting to delete images from filestack");
-                            const policyAndSignature = await encodeHandle(image.handle);
-                            const {policy, signature} = policyAndSignature.data;
-                            const filestackData = {
-                                key: token.token,
-                                handle: image.handle,
-                                policy,
-                                signature,
-                            }
-                            await deleteImageFromFilestack(filestackData);
+            /// deletes product images from the cloud
+            const deletedProduct = localData.filter(function (obj) {
+                return obj._id === productId;
+            });
+            if (deletedProduct[0].images.length !== 0) {
+                await Promise.all(deletedProduct[0].images.map(async function (image) {
+                    if (image.handle !== "sampleImage") {
+                        // console.log("attempting to delete images from filestack");
+                        const policyAndSignature = await encodeHandle(image.handle);
+                        const {policy, signature} = policyAndSignature.data;
+                        const filestackData = {
+                            key: token.token,
+                            handle: image.handle,
+                            policy,
+                            signature,
                         }
+                        await deleteImageFromFilestack(filestackData);
+                    }
 
-                    }));
-                }
-
-                /// deletes product from state
-                refetch();
-                // setLocalData(prevState => {
-                //     return prevState.filter(function (obj) {
-                //         return obj._id !== productId
-                //     });
-                // });
-            } catch (e) {
-                console.log(e);
+                }));
             }
+            /// deletes product from state
+            refetch();
+            // setLocalData(prevState => {
+            //     return prevState.filter(function (obj) {
+            //         return obj._id !== productId
+            //     });
+            // });
+        } catch (e) {
+            console.log(e);
         }
         dispatch(setLoading(false));
         completeEditHandler();
@@ -301,6 +299,10 @@ const AdminProductListPage = () => {
         // }
     }, [data?.products, localData]);
 
+   const openConfirmDeleteProductModal = () => {
+        window.alert_modal.showModal();
+   };
+
     return (
         isLoading || !localData ? <Spinner/> : error ? error : (
             <div className={"pt-10"}>
@@ -311,7 +313,7 @@ const AdminProductListPage = () => {
                             Products
                             <button
                                 onClick={() => navigate("/admin/products/create")}
-                                className={"absolute right-6 self-end btn btn-primary btn-xs lg:btn-sm"}
+                                className={"absolute right-6 self-end btn btn-primary btn-xs lg:btn-sm normal-case"}
                             >
                                 <FaPlus/>
                                 New Product
@@ -324,7 +326,7 @@ const AdminProductListPage = () => {
                             <thead>
                             <tr>
                                 <th/>
-                                <th>ID</th>
+                                {/*<th>ID</th>*/}
                                 <th className={"p-1"}>Name</th>
                                 <th className={"p-1"}>Brand</th>
                                 <th className={"p-1"}>Model</th>
@@ -346,9 +348,10 @@ const AdminProductListPage = () => {
                                                     editMode && item._id === productId ? (
                                                         <>
                                                             <th className={"bg-blue-200"}>{index+1}</th>
-                                                            <td className={"bg-blue-200"}>{item._id.substring(item._id.length - 6, item._id.length)}</td>
+                                                            {/*<td className={"bg-blue-200"}>{item._id.substring(item._id.length - 6, item._id.length)}</td>*/}
                                                             <td className={"p-1 bg-blue-200"}>
                                                                 <input
+                                                                    // className="pl-1 shadow appearance-none border rounded w-max text-gray-700 leading-tight focus:outline-none focus:shadow-primary"
                                                                     className="pl-1 py-2 shadow appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-primary"
                                                                     value={name}
                                                                     onChange={(e) => setName(e.target.value)}
@@ -372,7 +375,7 @@ const AdminProductListPage = () => {
                                                             </td>
                                                             <td className={"p-1 bg-blue-200"}>
                                                                 <input
-                                                                    className="pl-1 shadow appearance-none border rounded w-20 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-primary"
+                                                                    className="pl-1 shadow appearance-none border rounded w-[65px] py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-primary"
                                                                     type={"number"}
                                                                     min={0}
                                                                     value={price}
@@ -381,7 +384,7 @@ const AdminProductListPage = () => {
                                                             </td>
                                                             <td className={"p-1 bg-blue-200"}>
                                                                 <input
-                                                                    className="pl-1 shadow appearance-none border rounded w-12 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-primary"
+                                                                    className="pl-1 shadow appearance-none border rounded w-[40px] py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-primary"
                                                                     type={"number"}
                                                                     min={0}
                                                                     value={inStock}
@@ -403,18 +406,17 @@ const AdminProductListPage = () => {
                                                                     onChange={(e) => setDescription(e.target.value)}
                                                                 />
                                                             </td>
-                                                            <td className={"p-1 w-24 bg-blue-200"}>{item?.createdAt.substring(0, 10)}</td>
+                                                            <td className={"p-1 w-20 bg-blue-200"}>{item?.createdAt.substring(0, 10)}</td>
                                                             <td className={"p-1 bg-blue-200"}>
-
-                                                                <div className={"flex items-center"}>
+                                                                <div className={"flex justify-center items-center"}>
                                                                     <div className="tooltip tooltip-bottom" data-tip="save changes">
-                                                                        <button onClick={confirmUpdateHandler} className={"text-green-500 btn-glass btn-sm rounded-full"}>
-                                                                            <FaCheckCircle/>
+                                                                        <button onClick={confirmUpdateHandler} className={"text-green-500 btn-glass btn-xs rounded-full"}>
+                                                                            <FaCheckCircle className={"text-sm"}/>
                                                                         </button>
                                                                     </div>
                                                                     <div className="tooltip tooltip-bottom" data-tip="delete item">
-                                                                        <button onClick={submitDeleteProduct} className={"text-red-500 btn-glass btn-sm rounded-full"}>
-                                                                            <FaMinusCircle/>
+                                                                        <button onClick={() => openConfirmDeleteProductModal()} className={"text-red-500 btn-glass btn-xs rounded-full"}>
+                                                                            <FaMinusCircle className={"text-sm"}/>
                                                                         </button>
                                                                     </div>
                                                                 </div>
@@ -423,9 +425,9 @@ const AdminProductListPage = () => {
                                                     ) : (
                                                         <>
                                                         <th>{index+1}</th>
-                                                        <td>{item._id.substring(item._id.length - 6, item._id.length)}
-                                                        </td>
-                                                        <td className={"p-1"}><Link className={"link link-primary"} to={`/product/${item._id}`}>
+                                                        {/*<td>{item._id.substring(item._id.length - 6, item._id.length)}*/}
+                                                        {/*</td>*/}
+                                                        <td className={"p-1 w-64"}><Link className={"link link-primary"} to={`/product/${item._id}`}>
                                                             {item.name}
                                                         </Link></td>
                                                         <td className={"p-1"}>{item.brand}</td>
@@ -433,15 +435,21 @@ const AdminProductListPage = () => {
                                                         <td className={"p-1"}>${item.price}</td>
                                                         <td className={"p-1"}>{item.countInStock !== 0 ? item.countInStock : <FaTimes fill={"red"}/>}</td>
                                                         <td className={"p-1"}>{item.category}</td>
-                                                        <td className={"p-1"}>{item.description.substring(0, 32)}...</td>
-                                                        <td className={"w-24 p-1"}>{item?.createdAt.substring(0, 10)}</td>
-                                                        <td className={"p-1"}>
-                                                            <div className={"flex items-center"}>
-                                                                <button onClick={() => editProductHandler(item._id)} className={"btn-glass btn-sm rounded-full hover:text-primary"}>
-                                                                <FaEdit/>
+                                                        <td className={"p-1 w-64"}>{item.description.substring(0, 32)}...</td>
+                                                        <td className={"w-20 p-1"}>{item?.createdAt.substring(0, 10)}</td>
+                                                        <td className={"p-2"}>
+                                                            <div className={"flex justify-end items-center"}>
+                                                                <button
+                                                                    onClick={() => editProductHandler(item._id)}
+                                                                    className={"btn-glass btn-xs rounded-full hover:text-primary"}
+                                                                >
+                                                                    <FaEdit className={"text-sm"}/>
                                                                 </button>
-                                                                <button onClick={() => openImagesHandler(item._id)} className={"btn-glass btn-sm rounded-full hover:text-primary"}>
-                                                                    <FaImages/>
+                                                                <button
+                                                                    onClick={() => openImagesModal(item._id)}
+                                                                    className={`btn-glass btn-xs rounded-full hover:text-primary ${editMode && "hidden"}`}
+                                                                >
+                                                                    <FaImages className={"text-sm"}/>
                                                                 </button>
                                                             </div>
                                                         </td>
@@ -460,32 +468,43 @@ const AdminProductListPage = () => {
                             </tfoot>
                         </table>
                     </div>
-                    <div className={"pt-4 pb-8 flex justify-center"}>
+
+
+                    <div className={"pt-4 lg:pt-0 pb-8 flex justify-center"}>
                         <div className={"join"}>
                             <Paginate pages={data.pages} page={data.page} isAdmin={true}/>
                         </div>
                     </div>
+
                 </div>
+
+
+               {/*MODALS BELOW */}
+
+
                 <dialog id="confirm_modal" className="modal modal-bottom sm:modal-middle">
                     <form method="dialog" className="modal-box">
-                        <h3 className="p-4 font-bold text-lg">Please confirm these are the changes you wish to make.</h3>
-                        <div className="px-4">
+                        <div className={"flex justify-start items-center"}>
+                            <h3 className="p-3 font-bold text-xl">Confirm Changes</h3>
+                        </div>
+                        <h3 className="p-3 font-semibold text-lg">Please confirm these are the changes you wish to make --</h3>
+                        <div className="px-3">
                             {
                                 modalMessage !== "" && (
                                     modalMessage.split("&").map(function(sentence, index){
                                     //     let c = sentence.indexOf(":");
                                     // .substring(c+1, sentence.length)
                                         return (
-                                            <p className={"py-2"} key={index}>{sentence}</p>
+                                            <p className={"pt-2"} key={index}>{sentence}</p>
                                         )
                                     })
                                 )
                             }
                         </div>
                         <div className="modal-action">
-                            <button onClick={closeEditModal} className={"btn btn-error"}>Cancel</button>
+                            <button onClick={closeEditModal} className={"btn btn-neutral rounded-xl"}>Cancel</button>
                             <button
-                                className="btn btn-primary"
+                                className="btn rounded-xl"
                                 onClick={submitUpdateHandler}
                             >
                                 Confirm
@@ -535,8 +554,22 @@ const AdminProductListPage = () => {
                             </form>
                         )
                     }
-
                 </dialog>
+
+                <AlertModal title={"Delete Product"} initiateFunction={() => submitDeleteProduct()}>
+                    <div className={"flex flex-col"}>
+                        <p>
+                            Are you sure you want to delete this product?
+                            <span className={"pl-2 text-red-600 font-semibold"}>
+                                This cannot be undone.
+                            </span>
+                        </p>
+                        <p className={"pt-3 text-center font-bold text-lg"}>
+                            {name}
+                        </p>
+                    </div>
+                </AlertModal>
+
             </div>
         )
     );
