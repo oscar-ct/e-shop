3a7bcfa5825227002b0ba0a4e48b27861e1ffc54
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-07 23:52:34 -0500

    feat: adjust bg color and logo position

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -37,11 +37,11 @@ const SearchPage = () => {
                         data.products.length === 0 ? (
 
                             <div className={"absolute h-[calc(100vh-80px)] left-0 right-0 bottom-0"}>
-                                <div className={"w-full h-full"} style={{background: "url(https://cdn-icons-png.flaticon.com/512/380/380005.png?w=996&t=st=1691200957~exp=1691201557~hmac=b73c3f7d38c1498bb4bfba1e8bfbd260397b34152e8b60c4064cf9bf646b8b3f)", backgroundSize: "10%", backgroundRepeat: "no-repeat", backgroundPositionX: "right", backgroundPositionY: "bottom"}}>
+                                <div className={"w-full h-full"} style={{background: "url(https://cdn-icons-png.flaticon.com/512/380/380005.png?w=996&t=st=1691200957~exp=1691201557~hmac=b73c3f7d38c1498bb4bfba1e8bfbd260397b34152e8b60c4064cf9bf646b8b3f)", backgroundSize: "10%", backgroundRepeat: "no-repeat", backgroundPositionX: "center", backgroundPositionY: "center"}}>
                                     <h2 className={"text-3xl px-2 py-8 text-center"}>
                                         No search results for "{data.searchWord}"
                                     </h2>
-                                    <div  className={"m-auto bg-neutral rounded-xl max-w-[830px] shadow-xl"}>
+                                    <div  className={"m-auto bg-neutral/70 rounded-xl max-w-[830px] shadow-xl"}>
                                         {
                                             !windowResizing && (
                                                 <Snake />
