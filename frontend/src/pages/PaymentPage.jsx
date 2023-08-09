7256270919de41195227e2aeb0004f3403dbd32e
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-09 14:49:16 -0500

    feat: remove cc method, update paypal icon, minor style updates

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -4,8 +4,8 @@ import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {savePaymentMethod} from "../slices/cartSlice";
 import CheckoutSteps from "../components/CheckoutSteps";
-import {ReactComponent as PayPal} from "../icons/paypal.svg";
-import {FaCreditCard} from "react-icons/fa";
+import {ReactComponent as PayPal} from "../icons/paypal-icon.svg";
+// import {FaCreditCard} from "react-icons/fa";
 
 const PaymentPage = () => {
 
@@ -32,7 +32,7 @@ const PaymentPage = () => {
         e.preventDefault();
         if (paymentMeth) {
             dispatch(savePaymentMethod(paymentMeth));
-            navigate("/checkout")
+            navigate("/submitorder")
         }
 
     }
@@ -42,21 +42,21 @@ const PaymentPage = () => {
             <div className={"w-full flex justify-center"}>
                 <div className={"mb-10 card p-10 w-[35em] bg-base-100 shadow-xl"}>
                     <div className={"w-full pb-5 flex justify-center"}>
-                        <h1 className={"text-2xl text-neutral"}>Payment Methods</h1>
+                        <h1 className={"text-2xl text-neutral font-semibold"}>Payment Methods</h1>
                     </div>
                     <form onSubmit={submitPaymentMethod}>
-                        <div className="mt-5">
+                        <div className="my-5">
                             <div
                                 className={"w-full card bg-base-200 cursor-pointer"}
                                 onClick={() => setPaymentMeth("PayPal")}
                             >
                                 <div className={"w-full flex p-6"}>
                                     <div className={"w-2/12"}>
-                                        <PayPal className={"w-8"}/>
+                                      <PayPal width={"30"} height={"38"}/>
                                     </div>
                                     <div className={"w-8/12 flex  items-center"}>
                                         <span className={"text-lg"}>
-                                            PayPal
+                                            PayPal / Credit Card
                                         </span>
                                     </div>
                                     <div className={"w-2/12 flex items-center"}>
@@ -73,36 +73,36 @@ const PaymentPage = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="my-5">
-                            <div
-                                className={"w-full card bg-base-200 cursor-pointer"}
-                                onClick={() => setPaymentMeth("CreditCard")}
-                            >
-                                <div className={"w-full flex p-6"}>
-                                    <div className={"w-2/12"}>
-                                        <FaCreditCard className={"text-3xl"}/>
-                                    </div>
-                                    <div className={"w-8/12 flex  items-center"}>
-                                        <span className={"text-lg"}>
-                                            Credit Card
-                                        </span>
-                                    </div>
-                                    <div className={"w-2/12 flex items-center"}>
-                                        <input
-                                            onChange={(e) => setPaymentMeth(e.target.value)}
-                                            type="radio"
-                                            name="paymentMethod"
-                                            id={"CreditCard"}
-                                            value={"CreditCard"}
-                                            className="radio radio-primary"
-                                            checked={paymentMeth === "CreditCard"}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {/*<div className="my-5">*/}
+                        {/*    <div*/}
+                        {/*        className={"w-full card bg-base-200 cursor-pointer"}*/}
+                        {/*        onClick={() => setPaymentMeth("CreditCard")}*/}
+                        {/*    >*/}
+                        {/*        <div className={"w-full flex p-6"}>*/}
+                        {/*            <div className={"w-2/12"}>*/}
+                        {/*                <FaCreditCard className={"text-3xl"}/>*/}
+                        {/*            </div>*/}
+                        {/*            <div className={"w-8/12 flex  items-center"}>*/}
+                        {/*                <span className={"text-lg"}>*/}
+                        {/*                    Credit Card*/}
+                        {/*                </span>*/}
+                        {/*            </div>*/}
+                        {/*            <div className={"w-2/12 flex items-center"}>*/}
+                        {/*                <input*/}
+                        {/*                    onChange={(e) => setPaymentMeth(e.target.value)}*/}
+                        {/*                    type="radio"*/}
+                        {/*                    name="paymentMethod"*/}
+                        {/*                    id={"CreditCard"}*/}
+                        {/*                    value={"CreditCard"}*/}
+                        {/*                    className="radio radio-primary"*/}
+                        {/*                    checked={paymentMeth === "CreditCard"}*/}
+                        {/*                />*/}
+                        {/*            </div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                         <div className={"pt-5 w-full flex justify-end"}>
-                            <button className="btn btn-primary btn-wide rounded-xl " disabled={paymentMeth === null}>
+                            <button className="shadow-blue btn btn-primary btn-wide rounded-xl " disabled={paymentMeth === null}>
                                 Continue To Checkout
                             </button>
                         </div>
