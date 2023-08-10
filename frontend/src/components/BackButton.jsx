6ffd4dc8b4bf7e06a6cd392bbd9b5b00f64ba482
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-10 17:40:05 -0500

    update back btn

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 const BackButton = () => {
     return (
-        <Link className={"px-5 md:px-0 my-5 flex items-center w-min"} to={-1}>
+        <Link className={"px-2 md:px-0 my-5 flex items-center w-min"} to={-1}>
             <Arrow className={"w-5 h-5"}/>
             <span className={"pl-1 lg:text-sm font-normal"}>
                 BACK
