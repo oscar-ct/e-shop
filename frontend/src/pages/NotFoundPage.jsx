c6da10ffac2d3ebfe3ff5654db7edf020bc3a30a
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-10 11:55:10 -0500

    feat: create not found page

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,29 @@
+import BackButton from "../components/BackButton";
+
+const NotFoundPage = () => {
+    return (
+        <div className={"px-2"}>
+            <div className={"flex justify-start h-min"}>
+                <BackButton/>
+            </div>
+            <div className="hero">
+                <div className="text-center hero-content">
+                    <div className="max-w-lg">
+                        <h1 className="text-8xl font-bold mb-8"> 404
+                        </h1>
+                        <p className="text-3xl mb-8">
+                           Hmm, the page you were looking for doesn't seem to exist anymore
+                        </p>
+                        <div className={"pt-10 flex justify-center"}>
+                            <div>
+                                <img src={"/images/lazy-cat.webp"} alt={"Not found"} className={"max-w-96"}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
