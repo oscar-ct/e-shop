c6cb58636fc7692201c746a40f826f27ce9f0eb5
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-10 22:24:06 -0500

    feat: create meta helmet component

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Helmet} from "react-helmet-async";
+
+const Meta = ({title}) => {
+    return (
+        <Helmet>
+            <title>e-shop | {title}</title>
+            {/*<meta name={"description"} content={description}/>*/}
+            {/*<meta name={"keywords"} content={keywords}/>*/}
+        </Helmet>
+    );
+};
+
+Meta.defaultProps = {
+    title: "e-shop",
+}
+
+export default Meta;
