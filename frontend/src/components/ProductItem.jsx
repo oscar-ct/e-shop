dfea67fd3018ce36aecd788c41873b69856ef89d
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-14 23:56:38 -0500

    feat: set height to card, increase name string length

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -5,21 +5,21 @@ const ProductItem = ( {product} ) => {
     return (
         <>
             <div className="w-6/12 sm:w-72 p-2 sm:p-3">
-                <div className={"card bg-base-100 shadow-xl"}>
+                <div className={"card bg-base-100 shadow-xl h-full"}>
                     <Link to={`/product/${product._id}`}>
                         <figure className="p-2">
-                            <img src={product.images.length !== 0 ? product.images[0].url : "/images/sample.jpg"} alt="product" className="bg-zinc-100/70 w-[248px] h-[197px] object-scale-down rounded-tr-xl rounded-tl-xl" />
+                            <img src={product.images.length !== 0 ? product.images[0].url : "/images/sample.jpg"} alt="product" className="bg-zinc-100/20 w-[248px] h-[197px] object-scale-down rounded-tr-xl rounded-tl-xl" />
                         </figure>
                     </Link>
-                    <div className="card-body p-2 sm:p-4 items-center ">
-                        <div className={"w-full"}>
-                            <h2 className="text-sm truncate sm:text-lg font-bold">{product.name}</h2>
+                    <div className="card-body p-2 sm:p-4 items-center">
+                        <div className={"w-full h-[50px]"}>
+                            <h2 className="text-sm font-semibold">{product.name.length > 65 ? `${product.name.substring(0, 65)}...` : product.name}</h2>
                         </div>
 
                         {/*<p>{product.description}</p>*/}
                         <div className={"w-full flex flex-col"}>
                             <div className={"hidden sm:flex justify-start"}>
-                                <Rating rating={product.rating} text={`${product.numReviews} reviews`}/>
+                                <Rating rating={product.rating} text={`${product.numReviews} ${product.numReviews !== 1 ? "reviews" : "review"}`}/>
                             </div>
                             <div className={"flex sm:pt-2 justify-end"}>
                                 <span className={"sm:text-xl font-bold text-slate-500"}>${product.price}</span>
