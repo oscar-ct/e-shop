4f37bfe52f7c4dd5d3438ac1a8ac7e389b8835b7
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-17 20:04:06 -0500

    feat: use prop on Link

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -10,7 +10,7 @@ const Paginate = ({pages, page, isAdmin = false, searchTerm = "", sortByTerm = "
                         // <Link className={`join-item btn ${pg + 1 === page && "bg-neutral text-base-100 hover:text-neutral"}`} key={index} to={!isAdmin ? searchTerm ? `/search/${searchTerm}/page/${pg+1}` : `/page/${pg + 1}` : `/admin/products/page/${pg + 1}`}>
                         //    {pg + 1}
                         // </Link>
-                       <Link className={`join-item btn ${pg + 1 === page && "bg-neutral text-base-100 active:text-neutral sm:hover:text-neutral"}`} key={index} to={!isAdmin && searchTerm && !sortByTerm? `/search/${searchTerm}/page/${pg+1}` : !isAdmin && !searchTerm && !sortByTerm ? `/page/${pg + 1}` : isAdmin && !searchTerm && !sortByTerm ? `/admin/products/page/${pg + 1}` : `/sort/${sortByTerm}/page/${pg+1}`}>
+                       <Link preventScrollReset={true} className={`join-item btn ${pg + 1 === page && "bg-neutral text-base-100 active:text-neutral sm:hover:text-neutral"}`} key={index} to={!isAdmin && searchTerm && !sortByTerm? `/search/${searchTerm}/page/${pg+1}` : !isAdmin && !searchTerm && !sortByTerm ? `/page/${pg + 1}` : isAdmin && !searchTerm && !sortByTerm ? `/admin/products/page/${pg + 1}` : `/sort/${sortByTerm}/page/${pg+1}`}>
                            {pg + 1}
                        </Link>
                     )
