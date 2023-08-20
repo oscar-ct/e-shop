f221b9ede5f8744a705fd48e6bc34f95db1ee766
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-19 22:43:40 -0500

    update text size

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -6,7 +6,7 @@ const BackButton = () => {
     return (
         <Link className={"px-2 md:px-0 py-5 flex items-center w-min"} to={-1}>
             <Arrow fill={"currentColor"} className={"w-5 h-5"}/>
-            <span className={"pl-1 lg:text-sm font-normal"}>
+            <span className={"pl-1 font-base"}>
                 BACK
             </span>
         </Link>
