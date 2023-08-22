ee7b78e4b4908719429f4488bfc7603bfff6e7b7
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-21 20:24:46 -0500

    feat: add order redux state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import {apiSlice} from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
 import authSliceReducer from "./slices/authSlice";
 import loadingSliceReducer from "./slices/loadingSlice";
+import orderSliceReducer from "./slices/orderSlice";
 
 const store = configureStore({
    reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
       cart: cartSliceReducer,
       auth: authSliceReducer,
       loading: loadingSliceReducer,
+      order: orderSliceReducer,
 
    },
    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
