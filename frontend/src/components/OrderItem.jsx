ecbe423d971c6b3bf657a5f6a8c1095a3c69b4f2
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-26 12:02:05 -0500

    feat: add function to differentiate items that required refund for future use

--- frontend/src/components/OrderItem.jsx
+++ frontend/src/components/OrderItem.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 import {formatPrice} from "../utils/formatPriceUtilis";
 
 
-const OrderItem = ( {item, canceledItems, isCanceled} ) => {
+const OrderItem = ( {item, canceledItems, isCanceled, paidAt} ) => {
 
     // const strikethrough = () => canceledItems.includes(item.productId) && "line-through opacity-70";
     const strikethrough = function () {
@@ -10,10 +10,13 @@ const OrderItem = ( {item, canceledItems, isCanceled} ) => {
             return "line-through";
         }
     };
+
+    // ${(canceledItems.some(e => e.productId === item.productId && e.canceledAt > paidAt)) && "bg-green-300/20 rounded-md"}
+
     return (
         <>
-            <div className={`flex w-full mt-5 ${(canceledItems.some(e => e.productId === item.productId) || isCanceled) && "opacity-70"}`}>
-
+            <div className={`flex w-full mt-5 ${(canceledItems.some(e => e.productId === item.productId) || isCanceled) && "opacity-70"}`}
+            >
                 <div className={"w-2/12"}>
                     <Link className={"rounded-md bg-zinc-100/70 w-full h-full flex justify-center items-center"} to={`/product/${item.productId}`}>
                         <img className={"max-h-[160px] object-scale-down rounded-md"} src={item.images.length !== 0 ? item.images[0].url : "/images/sample.jpg"} alt={"cartItem"}/>
