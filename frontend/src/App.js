005005195eae6e05dc82f9cafedf0a8e3187fe27
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-27 12:02:39 -0500

    feat: remove unused parent div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,33 +9,32 @@ function App() {
     const loading = useSelector(function (state) {
         return state.loading;
     });
-  return (
-      <>
-          <ScrollRestoration
+    return (
+        <>
+            <ScrollRestoration
               // getKey={(location, matches) => {
               //     const paths = ["/product/:id"];
               //     return paths.includes(location.pathname) ? location.pathname : location.key;
               // }}
-          />
-          <Toaster />
-          {
-              loading.loading && (
-                  <div className={"absolute z-10 w-full h-screen flex justify-center"}>
+            />
+            <Toaster />
+            {
+                loading.loading && (
+                    <div className={"absolute z-10 w-full h-screen flex justify-center"}>
                       <span className="loading loading-bars loading-lg"/>
-                  </div>
-              )
-          }
-          <div className={loading.loading ? "opacity-30" : ""}>
-              <Navbar/>
-              <div className={"min-h-[calc(100vh-80px)]"}>
-                  <div className={"container md:px-5 xl:px-10 m-auto min-h-[calc(100vh-80px)]"}>
+                    </div>
+                )
+            }
+            <div className={loading.loading ? "opacity-30" : undefined}>
+                <Navbar/>
+                {/*<div className={"min-h-[calc(100vh-80px)]"}>*/}
+                    <div className={"container md:px-5 xl:px-10 m-auto min-h-[calc(100vh-80px)]"}>
                       <Outlet/>
-                  </div>
-              </div>
-          </div>
-      </>
-
-  );
+                    </div>
+                {/*</div>*/}
+            </div>
+        </>
+    );
 }
 
 export default App;
