a0102e52cf16b6fa02848df9a29f39a218253fcb
Oscar Castro <oscar.a.castro818@gmail.com>
2023-08-27 19:27:51 -0500

    feat: display a second product image on hover

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,14 +1,29 @@
 import {Link} from 'react-router-dom'
 import Rating from "./Rating";
+import {useState, useEffect} from "react";
+
+
 
 const ProductItem = ( {product, smallSize = false, cardWidth = ""} ) => {
+    const [imgIndex, setImageIndex] = useState(0);
+    const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth);
+
+    useEffect(function () {
+        const setInnerWidth = () => {
+            setWindowInnerWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", setInnerWidth);
+        return () => {
+            window.removeEventListener("resize", setInnerWidth)
+        }
+    }, []);
     return (
         <>
             <div className={"w-6/12 sm:w-72 p-1 sm:p-3"}>
                 <div className={`${cardWidth && cardWidth} rounded-xl flex flex-col bg-white shadow-xl h-full`}>
-                    <Link to={`/product/${product._id}`}>
+                    <Link to={`/product/${product._id}`} onMouseEnter={() => (windowInnerWidth >= 500 && product.images.length > 1) && setImageIndex(product.images.length - (product.images.length - 1))} onMouseLeave={() => (windowInnerWidth >= 500 && product.image.length > 1) && setImageIndex(0)}>
                         <figure className="p-2">
-                            <img src={product.images.length !== 0 ? product.images[0].url : "/images/sample.jpg"} alt="product" className="bg-zinc-100/20 w-[248px] h-[197px] object-scale-down rounded-tr-xl rounded-tl-xl" />
+                            <img src={product.images.length !== 0 ? product.images[imgIndex].url : "/images/sample.jpg"} alt="product" className="bg-zinc-100/20 w-[248px] h-[197px] object-scale-down rounded-tr-xl rounded-tl-xl" />
                         </figure>
                     </Link>
                     <div className={`card-body p-2 sm:px-4 items-start h-full flex flex-col ${!smallSize ? "justify-between" : "justify-start"} `}>
