1d03f7fb5ec4ff94bbb16057845cc747ee4e02b3
Oscar Castro <oscar.a.castro818@gmail.com>
2023-09-05 22:28:04 -0500

    feat:split node modules using rollup  manualChunks to improve chunking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,18 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
     base: "/",
+    build: {
+        chunkSizeWarningLimit: 700,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    },
     plugins: [
         react(),
         tailwindcss(),
