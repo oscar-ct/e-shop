2c64b30068959485a1c5e201b08db503721d12ee
Oscar Castro <oscar.a.castro818@gmail.com>
2023-09-05 23:09:02 -0500

    feat: testing new approach to chunk splitting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,30 @@ import react from "@vitejs/plugin-react";
 import eslintPlugin from 'vite-plugin-eslint';
 import tailwindcss from "tailwindcss";
 import svgr from 'vite-plugin-svgr';
+import { dependencies } from './package.json';
+
+function renderChunks(deps) {
+    let chunks = {};
+    Object.keys(deps).forEach((key) => {
+        if (['react', 'react-router-dom', 'react-dom', 'filestack-js'].includes(key)) return;
+        chunks[key] = [key];
+    });
+    return chunks;
+}
 
 export default defineConfig({
     base: "/",
+    build: {
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-router-dom', 'react-dom', 'filestack-js'],
+                    ...renderChunks(dependencies),
+                },
+            },
+        },
+    },
     plugins: [
         react(),
         tailwindcss(),
