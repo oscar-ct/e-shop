4255bf70481df3dc460cf06d8e03fa142a7d98a0
Oscar Castro <oscar.a.castro818@gmail.com>
2023-09-15 17:57:05 -0500

    feat: removed styles to reduce duplicated code, passed styles through props

--- frontend/src/components/SelectMenuSort.jsx
+++ frontend/src/components/SelectMenuSort.jsx
@@ -1,56 +1,15 @@
 import Select from "react-select";
 import {useNavigate} from "react-router-dom";
 
-const SelectMenuSort = ({sortByTerm, filterTerm}) => {
+const SelectMenuSort = ({sortByTerm, filterTerm, customStyles}) => {
 
     const navigate = useNavigate();
-
     const options = [
         {value: "toprated", label: "Top Rated"},
         {value: "latest", label: "Most Recent"},
         {value: "price-asc", label: "Price: Low To High"},
         {value: "price-dsc", label: "Price: High To Low"},
     ];
-
-    const customStyles = {
-        control: (base) => ({
-            ...base,
-            cursor: "pointer",
-            fontSize: "14px"
-        }),
-        option: (base, {isDisabled, isFocused, isSelected}) => {
-            return {
-                ...base,
-                cursor: "pointer",
-                borderRadius: 1,
-                color: "black",
-                backgroundColor: isFocused
-                    ? "rgba(132,170,252,0.3)"
-                    : isSelected
-                        ? "rgb(132,166,252)"
-                        : "rgb(245,245,245)",
-                ':active': {
-                    ...base[':active'],
-                    backgroundColor: !isDisabled
-                        && isSelected
-                        && "rgb(245,245,245)",
-
-                },
-            };
-        },
-        menu: base => ({
-            ...base,
-            cursor: "pointer",
-            borderRadius: 5,
-        }),
-        menuList: base => ({
-            ...base,
-            padding: 0,
-            cursor: "pointer",
-            fontSize: "14px"
-        })
-    }
-
     const placeHolder = () => {
         if (sortByTerm === "toprated") {
             return "Top Rated"
@@ -61,11 +20,10 @@ const SelectMenuSort = ({sortByTerm, filterTerm}) => {
         } else if (sortByTerm === "price-dsc") {
             return "Price: High To Low"
         }
-    }
+    };
     const handleChange = (selectedOption) => {
         navigate(`/sort/${selectedOption.value}/${filterTerm}`)
     };
-
     return (
         <Select
             styles={customStyles}
