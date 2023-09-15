ff2885eaf7f60ecb15fef599f66ace7a1bbe0634
Oscar Castro <oscar.a.castro818@gmail.com>
2023-09-15 17:56:42 -0500

    feat: update category select menu list, removed styles to reduce duplicated code, passed styles through props

--- frontend/src/components/SelectMenuCategory.jsx
+++ frontend/src/components/SelectMenuCategory.jsx
@@ -1,71 +1,55 @@
 import Select from "react-select";
 import {useNavigate} from "react-router-dom";
 
-const SelectMenuCategory = ({sortByTerm, filterTerm}) => {
+const SelectMenuCategory = ({sortByTerm, filterTerm, customStyles}) => {
 
     const navigate = useNavigate();
-
     const options = [
         {value: "all", label: "All"},
+        {value: "apparel", label: "Apparel"},
+        {value: "automotive", label: "Automotive"},
         {value: "electronics", label: "Electronics"},
+        {value: "food", label: "Food"},
+        {value: "footwear", label: "Footwear"},
+        {value: "games", label: "Games"},
+        {value: "health", label: "Health"},
+        {value: "home", label: "Home"},
+        {value: "pet", label: "Pet"},
+        {value: "other", label: "Other"},
     ];
-
-    const customStyles = {
-        control: (base) => ({
-            ...base,
-            cursor: "pointer",
-            fontSize: "14px"
-        }),
-        option: (base, {isDisabled, isFocused, isSelected}) => {
-            return {
-                ...base,
-                cursor: "pointer",
-                borderRadius: 1,
-                color: "black",
-                backgroundColor: isFocused
-                    ? "rgba(132,170,252,0.3)"
-                    : isSelected
-                        ? "rgb(132,166,252)"
-                        : "rgb(245,245,245)",
-                ':active': {
-                    ...base[':active'],
-                    backgroundColor: !isDisabled
-                        && isSelected
-                        && "rgb(245,245,245)",
-
-                },
-            };
-        },
-        menu: base => ({
-            ...base,
-            cursor: "pointer",
-            borderRadius: 5,
-        }),
-        menuList: base => ({
-            ...base,
-            padding: 0,
-            cursor: "pointer",
-            fontSize: "14px"
-        })
-    }
-
-    const placeHolder = () => {
-        if (filterTerm === "all") {
-            return "All"
-        } else if (filterTerm === "electronics") {
-            return "Electronics"
+    const placeHolder = (category) => {
+        switch (category) {
+            case "all":
+                return "All"
+            case "apparel":
+                return "Apparel"
+            case "automotive":
+                return "Automotive"
+            case "electronics":
+                return "Electronics"
+            case "food":
+                return "Food"
+            case "footwear":
+                return "Footwear"
+            case "games":
+                return "Games"
+            case "home":
+                return "Home"
+            case "pet":
+                return "Pet"
+            case "other":
+                return "Other"
         }
-    }
+    };
     const handleChange = (selectedOption) => {
         navigate(`/sort/${sortByTerm}/${selectedOption.value}`)
     };
-
     return (
         <Select
             styles={customStyles}
             isSearchable={false}
             className={"text-black w-[12em]"}
-            placeholder={placeHolder()}
+            placeholder={placeHolder(filterTerm)}
             options={options}
             onChange={handleChange}
         />
