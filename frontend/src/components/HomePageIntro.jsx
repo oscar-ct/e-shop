bfdf2c7e564e8d81738d0e51c79a570b8cb7780a
Oscar Castro <oscar.a.castro818@gmail.com>
2024-01-16 18:23:58 -0600

    feat: revamp homepage intro with react swiper

--- frontend/src/components/HomePageIntro.jsx
+++ frontend/src/components/HomePageIntro.jsx
@@ -1,14 +1,22 @@
 import {ReactComponent as Logo} from "../icons/e.svg";
 import {Link} from "react-router-dom";
+import { Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay, Pagination, EffectFade} from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-fade';
+import {FaCcPaypal} from "react-icons/fa";
 
 const HomePageIntro = () => {
     return (
-        <div className={"drop-shadow-xl bg-transparent rounded-xl w-full"}>
+        <div className={"lg:drop-shadow-xl bg-transparent"}>
+
+            {/*MOBILE*/}
             <div className={"md:hidden w-full h-full relative"}>
                 <div className={"absolute h-full w-full flex flex-col items-center justify-start ibmplex"}>
                     <Logo className={"pt-28 w-[10em]"}/>
                     <span className={"pt-12 text-3xl font-semibold z-10"}>Shop, Ship, & Enjoy.</span>
-                    {/*<span className={"pt-3 font-light pb-20 text-2xl"}>Happy Holidays!</span>*/}
                     <div className={"pt-20"}>
                         <Link to={"/sort/latest/select/all"} className={"btn text-lg btn-neutral normal-case rounded-full"}>
                             Get Started
@@ -17,39 +25,76 @@ const HomePageIntro = () => {
                 </div>
                 <img className={"object-cover h-[40em] w-full"} src={"/images/bg.png"}/>
             </div>
-            <div className={"hidden md:flex h-[35em] w-full ibmplex"}>
-                <div
-                    style={{backgroundImage: `url(/images/sincerely-media-HL3EOgFiy0k-unsplash.jpg)`, backgroundPosition: "center", backgroundSize: "cover"}}
-                    className={"h-full w-1/3"}
-                >
-                    <div className={"w-full h-full flex items-end justify-center"}>
-                        <div className={"pb-10 text-3xl font-bold flex items-center"}>
-                            <Logo width={"20px"} className={"pt-1 mr-1"}/>
-                            -shop.com
+            {/*MOBILE*/}
+
+            {/*DESKTOP*/}
+            <div className={"hidden md:block w-full"}>
+                <div className={"flex h-[32em] w-full ibmplex"}>
+                    <div
+                        style={{backgroundImage: `url(/images/sincerely-media-HL3EOgFiy0k-unsplash.jpg)`, backgroundPosition: "center", backgroundSize: "cover"}}
+                        className={"h-full w-1/3"}
+                    >
+                        <div className={"w-full h-full flex items-end justify-center"}>
+                            <div className={"pb-10 text-3xl font-bold flex items-center"}>
+                                <Logo width={"20px"} className={"pt-1 mr-1"}/>
+                                -shop.com
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div
-                    style={{backgroundImage: `url(/images/curology-fPSELOXfeU4-unsplash.jpg)`, backgroundPosition: "center", backgroundSize: "cover"}}
-                    className={"h-full w-1/3"}
-                >
-                    <div className={"w-full h-full flex items-start justify-center"}>
-                        <div className={"pt-20 px-10 font-bold text-white text-3xl"}>
-                            Shop, Ship, & Enjoy.
+                    <div
+                        style={{backgroundImage: `url(/images/curology-fPSELOXfeU4-unsplash.webp)`, backgroundPosition: "center", backgroundSize: "cover"}}
+                        className={"h-full w-1/3"}
+                    >
+                        <div className={"w-full h-full flex items-start justify-center"}>
+                            <div className={"pt-20 px-10 font-bold text-white text-3xl"}>
+                                Shop, Ship, & Enjoy.
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div
-                    style={{backgroundImage: `url(/images/ian-dooley-hpTH5b6mo2s-unsplash.jpg)`, backgroundPosition: "center", backgroundSize: "cover"}}
-                    className={"h-full w-1/3"}
-                >
-                    <div className={"w-full h-full flex items-center justify-center"}>
-                        <Link to={"/sort/latest/select/all"} className={"btn text-lg btn-neutral normal-case rounded-full"}>
-                            Get Started
-                        </Link>
+                    <div className={"w-1/3 h-full"}>
+                        <Swiper
+                            pagination
+                            slidesPerView={1}
+                            autoplay={{
+                                delay: 8500,
+                                disableOnInteraction: false
+                            }}
+                            modules={[Autoplay, EffectFade, Pagination]}
+                            effect={"fade"}
+                            fadeEffect={{crossFade: true}}
+                        >
+                            <SwiperSlide>
+                                <div
+                                    style={{backgroundImage: `url(/images/ian-dooley-hpTH5b6mo2s-unsplash.jpg)`, backgroundPosition: "center", backgroundSize: "cover",}}
+                                    className={"h-[32em]"}
+                                >
+                                    <div className={"w-full h-full flex items-center justify-center"}>
+                                        <Link to={"/sort/latest/select/all"} className={"btn text-lg btn-neutral normal-case rounded-sm"}>
+                                            Get Started
+                                        </Link>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div
+                                    style={{backgroundImage: `url(/images/muhammad-asyfaul-f2tNsobMisQ-unsplash.jpg)`, backgroundPosition: "center", backgroundSize: "cover",}}
+                                    className={"h-[32em]"}
+                                >
+                                    <div className={"w-full h-full flex items-end justify-end"}>
+
+                                        <div className={"p-10 flex flex-col items-center"}>
+                                            <span className={"p-2 text-3xl text-white font-bold"}>Shop worry-free with</span>
+                                            <FaCcPaypal color={"white"} size={"3em"}/>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
+            {/*DESKTOP*/}
         </div>
     );
 };
