b2e5c17018accff262948303ccfc58d1a06cd910
Oscar Castro <oscar.a.castro818@gmail.com>
2024-01-17 19:01:35 -0600

    feat: clean up text

--- frontend/src/components/CheckoutItem.jsx
+++ frontend/src/components/CheckoutItem.jsx
@@ -63,9 +63,9 @@ const CheckoutItem = ( {item} ) => {
                         <div className={"w-3/12 flex justify-end items-end"}>
 
                             <div className={"flex justify-end items-center flex-col lg:flex-row"}>
-                                <label className="block lg:mr-2 text-sm font-medium text-gray-900 dark:text-white pb-3">
-                                    Qty:
-                                </label>
+                                {/*<label className="block lg:mr-2 text-sm font-medium text-gray-900 dark:text-white pb-3">*/}
+                                {/*    Qty:*/}
+                                {/*</label>*/}
                                 <select
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     value={item.quantity}
