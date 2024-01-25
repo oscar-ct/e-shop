7a382cd5aa869eb335f187988a6704730db17c3a
Oscar Castro <oscar.a.castro818@gmail.com>
2024-01-24 20:55:03 -0600

    feat: improve redux state price calculation

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -1,14 +1,20 @@
+function addDecimals(num) {
+    return (Math.round(num * 100) / 100).toFixed(2);
+}
+
 export const updateCart = (state) => {
     if (state.cartItems.length !== 0) {
-        state.itemsPrice = state.cartItems.reduce(function (acc, item) {
+        state.itemsPrice = addDecimals(state.cartItems.reduce(function (acc, item) {
             return (acc + item.price * item.quantity);
-        }, 0);
+        }, 0));
 
         state.shippingPrice =  state.discount ? 0 : Number(Math.round(state.itemsPrice > 100 ? 0 : 10).toFixed(2));
 
-        state.taxPrice = Number((0.0825 * state.itemsPrice).toFixed(2));
+        const taxableAmount = state.itemsPrice + state.shippingPrice;
 
-        state.totalPrice = (Number(Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2));
+        state.taxPrice = addDecimals(Number((0.0825 * taxableAmount).toFixed(2)));
+
+        state.totalPrice = addDecimals(Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice));
     } else {
         state.itemsPrice = 0;
         state.shippingPrice = 0;
@@ -17,7 +23,3 @@ export const updateCart = (state) => {
     }
     localStorage.setItem("cart", JSON.stringify(state));
 }
-
-// const addDecimals = (int) => {
-//    return (Math.round(int * 100) / 100).toFixed(2);
-// }
