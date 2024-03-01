ac0c897caaaaabc251efc338b12e24587434d293
Oscar Castro <oscar.a.castro818@gmail.com>
2024-02-29 18:50:10 -0600

    feat: update nav cart icon to display on hover using react state and useRef hook

--- frontend/src/components/CartIcon.jsx
+++ frontend/src/components/CartIcon.jsx
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 
 const CartIcon = ({cartItems, totalCartItems, subtotalPrice}) => {
 
     const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth);
+    const [cartDropdownActive, setCartDropdownActive] = useState(false);
+    const documentRef5 = useRef();
 
     useEffect(function () {
         const setInnerWidth = () => {
@@ -14,6 +16,18 @@ const CartIcon = ({cartItems, totalCartItems, subtotalPrice}) => {
             window.removeEventListener("resize", setInnerWidth)
         }
     }, []);
+    useEffect(function () {
+        const closeUserOpenDropdown = (e) => {
+            if (!documentRef5?.current?.contains(e.target)) {
+                setCartDropdownActive(false);
+            }
+        }
+        if (cartDropdownActive) {
+            window.addEventListener('mouseout', closeUserOpenDropdown);
+        }
+        return () => window.removeEventListener("mouseout", closeUserOpenDropdown);
+
+    }, [cartDropdownActive]);
 
     return (
         <>
@@ -27,13 +41,12 @@ const CartIcon = ({cartItems, totalCartItems, subtotalPrice}) => {
                                     <span className="badge text-white bg-primary badge-sm indicator-item">{totalCartItems}</span>
                                 )
                             }
-
                         </div>
                         <span className={"hidden md:flex normal-case"}>Cart</span>
                     </Link>
                 ) : (
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="px-2 btn btn-ghost">
+                    <div  ref={documentRef5} className="dropdown dropdown-end py-4">
+                        <label onMouseEnter={() => setCartDropdownActive(prevState => !prevState)} className="px-2 btn btn-ghost">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 {
@@ -45,35 +58,33 @@ const CartIcon = ({cartItems, totalCartItems, subtotalPrice}) => {
                             </div>
                             <span className={"normal-case"}>Cart</span>
                         </label>
-                        <div tabIndex={0} className="mt-2 z-[1] card card-compact dropdown-content w-52 bg-neutral/70 shadow">
-                            <div className="card-body">
-                                {
-                                    cartItems.length !== 0 ? (
-                                        <>
-                                    <span className="font-bold text-white text-lg">
-                                        ({totalCartItems}) {totalCartItems > 1 ? "items" : "item"}
-                                    </span>
-                                            <span className="font-bold text-info">
-                                        Subtotal:
-                                        <span className={"pl-2 text-white"}>
-                                            ${subtotalPrice}
-                                        </span>
-                                    </span>
-                                            <div className="card-actions">
-                                                <Link to={"/cart"} className="btn btn-primary btn-block text whitespace-nowrap uppercase rounded-md">
-                                                    View Cart
-                                                </Link>
-                                            </div>
-                                        </>
-                                    ) : (
-                                        <span className={"font-bold text-white text-center text-sm"}>
+                        {
+                            cartDropdownActive && (
+                                <div className="mt-4 z-10 absolute right-0 origin-top-right w-52 bg-neutral/70 shadow rounded-b-md">
+                                    <div className="p-5">
+                                        {
+                                            cartItems.length !== 0 ? (
+                                                <>
+                                                    <span className="font-bold text-white text-xl">({totalCartItems}) {totalCartItems > 1 ? "Items" : "Item"}</span>
+                                                    <div className="font-bold text-info py-3">
+                                                        Subtotal:<span className={"pl-2 text-white"}>${subtotalPrice}</span>
+                                                    </div>
+                                                    <Link to={"/cart"} className="btn btn-primary btn-block text whitespace-nowrap uppercase rounded-md">
+                                                        View Cart
+                                                    </Link>
+                                                </>
+                                            ) : (
+                                                <span className={"font-bold text-white text-center text-xl"}>
                                     Your cart is empty...
                                 </span>
-                                    )
-                                }
+                                            )
+                                        }
 
-                            </div>
-                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+
                     </div>
                 )
             }
