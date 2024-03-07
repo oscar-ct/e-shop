945a314124da69845f95ee5771a8d2f5153e5574
Oscar Castro <oscar.a.castro818@gmail.com>
2024-03-07 14:26:01 -0600

    style: checkout steps, update font and bg color

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -10,80 +10,80 @@ const CheckoutSteps = ({ step1, step2, step3 }) => {
     const step1name = "Cart"
     const step2name = "Shipping"
     const step3name = "Payment"
-    const step4name = "Checkout"
+    const step4name = "Place Order"
 
     return (
         <div className={"flex justify-center pt-5 lg:pt-8 text-xs sm:text-sm"}>
             {
                 step1 ? (
                     <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-primary md:step-neutral">{step1name}</Link>
+                        <Link to={"/cart"} className="step step-neutral font-semibold ibmplex">{step1name}</Link>
                         {
                             cartItems.length !== 0 ? (
-                                <Link to={"/shipping"} className="step">{step2name}</Link>
+                                <Link to={"/shipping"} className="step"/>
                             ) : (
-                                <button className="step">{step2name}</button>
+                                <button className="step font-semibold ibmplex">{step2name}</button>
                             )
                         }
                         {
                             Object.keys(shippingAddress).length !== 0 ? (
-                                <Link to={"/payment"} className="step">{step3name}</Link>
+                                <Link to={"/payment"} className="step"/>
                             ) : (
-                                <button className="step">{step3name}</button>
+                                <button className="step"/>
                             )
                         }
                         {
                             paymentMethod ? (
-                                <Link to={"/submitorder"} className="step">{step4name}</Link>
+                                <Link to={"/submitorder"} className="step"/>
                             ) : (
-                                <button className="step">{step4name}</button>
+                                <button className="step"/>
                             )
                         }
                     </div>
                 ) : step2 ? (
                     <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-primary md:step-neutral">{step1name}</Link>
+                        <Link to={"/cart"} className="step step-neutral"/>
                         {
                             cartItems.length !== 0 ? (
-                                <Link to={"/shipping"} className="step step-primary md:step-neutral">{step2name}</Link>
+                                <Link to={"/shipping"} className="step step-neutral font-semibold ibmplex">{step2name}</Link>
                             ) : (
-                                <button className="step step-primary md:step-neutral">{step2name}</button>
+                                <button className="step step-neutral font-semibold ibmplex">{step2name}</button>
                             )
                         }
                         {
                             Object.keys(shippingAddress).length !== 0 ? (
-                                <Link to={"/payment"} className="step">{step3name}</Link>
+                                <Link to={"/payment"} className="step"/>
                             ) : (
-                                <button className="step mx-2">{step3name}</button>
+                                <button className="step mx-2"/>
                             )
                         }
                         {
                             paymentMethod ? (
-                                <Link to={"/submitorder"} className="step">{step4name}</Link>
+                                <Link to={"/submitorder"} className="step"/>
                             ) : (
-                                <button className="step">{step4name}</button>
+                                <button className="step"/>
                             )
                         }
                     </div>
                 ) : step3 ?  (
                     <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-primary md:step-neutral">{step1name}</Link>
-                        <Link to={"/shipping"} className="step step-primary md:step-neutral">{step2name}</Link>
-                        <Link to={"/payment"} className="step step-primary md:step-neutral">{step3name}</Link>
+                        <Link to={"/cart"} className="step step-neutral"/>
+                        <Link to={"/shipping"} className="step step-neutral"/>
+                        <Link to={"/payment"} className="step step-neutral font-semibold ibmplex">{step3name}</Link>
                         {
                             paymentMethod ? (
-                                <Link to={"/submitorder"} className="step">{step4name}</Link>
+                                <Link to={"/submitorder"} className="step"/>
                             ) : (
-                                <button className="step">{step4name}</button>
+                                <button className="step"/>
                             )
                         }
                     </div>
                 ) : (
                     <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-primary md:step-neutral">{step1name}</Link>
-                        <Link to={"/shipping"} className="step step-primary md:step-neutral">{step2name}</Link>
-                        <Link to={"/payment"} className="step step-primary md:step-neutral">{step3name}</Link>
-                        <Link to={"/submitorder"} className="step step-primary md:step-neutral">{step4name}</Link>
+                        <Link to={"/cart"} className="step step-neutral"/>
+                        <Link to={"/shipping"} className="step step-neutral"/>
+                        <Link to={"/payment"} className="step step-neutral"/>
+                        <Link to={"/submitorder"} className="step step-neutral font-semibold ibmplex">{step4name}</Link>
                     </div>
                 )
             }
