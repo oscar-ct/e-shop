f0a8d4f0f17192a2d8c9981dd045dc2aea0e34c0
Oscar Castro <oscar.a.castro818@gmail.com>
2024-03-09 18:17:46 -0600

    feat: revamp checkout steps with colors and icons

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -1,92 +1,65 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {useSelector} from "react-redux";
 
 const CheckoutSteps = ({ step1, step2, step3 }) => {
 
-    const {cartItems,shippingAddress, paymentMethod} = useSelector(function (state) {
+    const {shippingAddress, paymentMethod} = useSelector(function (state) {
        return state.cart;
     });
+
+    const {pathname} = useLocation();
 
     const step1name = "Cart"
     const step2name = "Shipping"
     const step3name = "Payment"
     const step4name = "Place Order"
 
+    const isCartPage = pathname.includes(step1name.toLowerCase());
+    const isShippingPage = pathname.includes(step2name.toLowerCase());
+    const isPaymentPage = pathname.includes(step3name.toLowerCase());
+    const isPlaceOrderPage = pathname.includes("submitorder");
+
     return (
         <div className={"flex justify-center pt-5 lg:pt-8 text-xs sm:text-sm"}>
-            {
-                step1 ? (
-                    <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-neutral font-semibold ibmplex">{step1name}</Link>
-                        {
-                            cartItems.length !== 0 ? (
-                                <Link to={"/shipping"} className="step"/>
-                            ) : (
-                                <button className="step font-semibold ibmplex">{step2name}</button>
-                            )
-                        }
+                    <div className={"steps w-full ibmplex"}>
+                        <Link to={"/cart"} data-content="✓" className={`step step-success ${isCartPage ? "text-xl" : "text-gray-400"}`}>
+                            {step1name}
+                        </Link>
                         {
                             Object.keys(shippingAddress).length !== 0 ? (
-                                <Link to={"/payment"} className="step"/>
+                                <Link data-content="✓" to={"/shipping"} className={`step step-success ${isShippingPage ? "text-xl" : "text-gray-400"}`}>
+                                    {step2name}
+                                </Link>
                             ) : (
-                                <button className="step"/>
+                                <Link data-content="✕" to={"/shipping"} className={`step step-error ${isShippingPage ? "text-xl" : "text-gray-400"}`}>
+                                    {step2name}
+                                </Link>
                             )
                         }
                         {
                             paymentMethod ? (
-                                <Link to={"/submitorder"} className="step"/>
+                                <Link data-content="✓" to={"/payment"} className={`step step-success ${isPaymentPage ? "text-lg sm:text-xl" : "text-gray-400"}`}>
+                                    {step3name}
+                                </Link>
                             ) : (
-                                <button className="step"/>
-                            )
-                        }
-                    </div>
-                ) : step2 ? (
-                    <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-neutral"/>
-                        {
-                            cartItems.length !== 0 ? (
-                                <Link to={"/shipping"} className="step step-neutral font-semibold ibmplex">{step2name}</Link>
-                            ) : (
-                                <button className="step step-neutral font-semibold ibmplex">{step2name}</button>
+                                <button data-content="✕" className={`step step-error ${isPaymentPage ? "text-lg sm:text-xl" : "text-gray-400"}`}>
+                                    {step3name}
+                                </button>
                             )
                         }
                         {
-                            Object.keys(shippingAddress).length !== 0 ? (
-                                <Link to={"/payment"} className="step"/>
+                            Object.keys(shippingAddress).length !== 0 && paymentMethod ? (
+                                <Link data-content="★" to={"/submitorder"} className={`step step-warning ${isPlaceOrderPage ? "text-lg sm:text-xl" : "text-gray-400"}`}>
+                                    {step4name}
+                                </Link>
                             ) : (
-                                <button className="step mx-2"/>
+                                <button data-content="★" className={`step step-error ${isPlaceOrderPage ? "text-lg sm:text-xl" : "text-gray-400"}`}>
+                                    {step4name}
+                                </button>
                             )
                         }
-                        {
-                            paymentMethod ? (
-                                <Link to={"/submitorder"} className="step"/>
-                            ) : (
-                                <button className="step"/>
-                            )
-                        }
+
                     </div>
-                ) : step3 ?  (
-                    <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-neutral"/>
-                        <Link to={"/shipping"} className="step step-neutral"/>
-                        <Link to={"/payment"} className="step step-neutral font-semibold ibmplex">{step3name}</Link>
-                        {
-                            paymentMethod ? (
-                                <Link to={"/submitorder"} className="step"/>
-                            ) : (
-                                <button className="step"/>
-                            )
-                        }
-                    </div>
-                ) : (
-                    <div className={"steps w-full"}>
-                        <Link to={"/cart"} className="step step-neutral"/>
-                        <Link to={"/shipping"} className="step step-neutral"/>
-                        <Link to={"/payment"} className="step step-neutral"/>
-                        <Link to={"/submitorder"} className="step step-neutral font-semibold ibmplex">{step4name}</Link>
-                    </div>
-                )
-            }
         </div>
     );
 };
