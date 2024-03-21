01873137a325a75b252440ae9729048d09943e72
Oscar Castro <oscar.a.castro818@gmail.com>
2024-03-21 12:41:14 -0500

    create new reveal effect with framer motion

---
+++ frontend/src/components/Reveal.jsx
@@ -0,0 +1,47 @@
+import {motion, useAnimation, useInView} from "framer-motion";
+import {useEffect, useRef} from "react";
+
+const Reveal = ({children, customClass, delay, once}) => {
+
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once: once});
+
+    const mainControls = useAnimation();
+    useEffect(() => {
+        if (isInView) {
+            mainControls.start("visible")
+        }
+        if (!once && !isInView) {
+            mainControls.start("hidden")
+        }
+    }, [isInView, mainControls])
+
+    return (
+        <div ref={ref}>
+            <motion.div
+                variants={{
+                    hidden: { opacity: 0, y: 75},
+                    visible: {opacity: 1, y: 0},
+                }}
+                initial={"hidden"}
+                animate={mainControls}
+                transition={{
+                    duration: 0.55,
+                    delay: delay
+                }}
+                exit={"hidden"}
+                className={`${customClass}`}
+            >
+                {children}
+            </motion.div>
+        </div>
+    );
+};
+
+Reveal.defaultProps = {
+    delay: 0.0,
+    once: true,
+}
+
+
+export default Reveal;
