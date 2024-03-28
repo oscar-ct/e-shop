3f9698143a45511957828502783a580ed5a817ee
Oscar Castro <oscar.a.castro818@gmail.com>
2024-03-28 00:46:48 -0500

    remove placeholders add bg color

--- frontend/src/pages/AdminCreateProductPage.jsx
+++ frontend/src/pages/AdminCreateProductPage.jsx
@@ -271,7 +271,6 @@ const AdminCreateProductPage = () => {
                                                         className={`bg-white w-full text-base px-4 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-400 ${newProduct && "border-none bg-base-100 font-semibold"}`}
                                                         autoComplete={"off"}
                                                         type={"number"}
-                                                        placeholder={"Product Brand"}
                                                         id={"countInStock"}
                                                         value={formData.countInStock}
                                                         onChange={onMutate}
@@ -284,10 +283,9 @@ const AdminCreateProductPage = () => {
                                                     <label className="text-sm font-medium text-gray-700 tracking-wide">List Price
                                                     </label>
                                                     <input
-                                                        className={`w-full text-base px-4 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-400 ${newProduct && "border-none bg-base-100 font-semibold"}`}
+                                                        className={`bg-white w-full text-base px-4 py-2 border border-gray-300 rounded-sm focus:outline-none focus:border-blue-400 ${newProduct && "border-none bg-base-100 font-semibold"}`}
                                                         autoComplete={"off"}
                                                         type={"number"}
-                                                        placeholder={"Product Model"}
                                                         id={"price"}
                                                         value={formData.price}
                                                         onChange={onMutate}
