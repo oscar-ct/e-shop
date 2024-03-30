f53521b4e7c33befa768f6f74bdee63c09a5568c
Oscar Castro <oscar.a.castro818@gmail.com>
2024-03-30 15:40:55 -0500

    minor style update, adjust padding and bg colors

--- frontend/src/components/HomePageCategorySection.jsx
+++ frontend/src/components/HomePageCategorySection.jsx
@@ -12,16 +12,16 @@ const HomePageCategorySection = ({productCategories, windowInnerWidth}) => {
 
     return (
         <>
-            <div className={"pb-10 pt-14 md:bg-transparent bg-white w-full"}>
+            <div className={"pb-10 pt-14 md:pt-0 md:bg-transparent bg-white w-full"}>
                 <div className={"h-12 md:bg-zinc-700 border-none md:border-b-[1px] md:border-grey-300"}>
-                    <div  className={"flex justify-center lg:justify-start items-center h-full w-full"}>
+                    <div  className={"flex justify-center items-center h-full w-full"}>
                         <h2 className={"lg:pl-3 text-3xl md:text-2xl md:text-white ibmplex"}>
                             Popular Categories
                         </h2>
                     </div>
                 </div>
 
-                <div className={"w-full flex flex-wrap justify-center md:border py-10"}>
+                <div className={"bg-white w-full flex flex-wrap justify-center md:border py-6"}>
                     {
                         productCategories.slice(0, !categoryDropdownActive && windowInnerWidth < 768 ? 6 : !categoryDropdownActive && windowInnerWidth < 1024 && windowInnerWidth >= 768 ? 4 : !categoryDropdownActive && windowInnerWidth > 1024 && windowInnerWidth < 1280 ? 5 : !categoryDropdownActive && windowInnerWidth >= 1280 && windowInnerWidth < 1282 ? 6 :
                             !categoryDropdownActive && windowInnerWidth >= 1282 && windowInnerWidth < 1536 ? 7 :
@@ -30,13 +30,13 @@ const HomePageCategorySection = ({productCategories, windowInnerWidth}) => {
                         })
                     }
                 </div>
-                <div className={"lg:pt-5"}>
+                <div className={"md:pt-3"}>
                     <div className={"flex justify-end items-center px-2"}>
                         <div>
                             {/*<Link to={"/sort/latest/select/all"} className={"btn glass bg-neutral/70 text-white"}>*/}
                             {/*    View All Categories*/}
                             {/*</Link>*/}
-                            <button onClick={() => setCategoryDropdownActive(!categoryDropdownActive)} className={"flex items-center text-lg text-black"}>
+                            <button onClick={() => setCategoryDropdownActive(!categoryDropdownActive)} className={"flex items-center text-black hover:text-black/70"}>
                                 {categoryDropdownActive ? "Less Categories" : "More Categories"}
                                 <div className={`px-2 ${rotateChevron(categoryDropdownActive)}`}>
                                     <FaChevronDown/>
