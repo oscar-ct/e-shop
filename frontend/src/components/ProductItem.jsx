5909bf37188fd28cf687d2d61d7fed80dc2cccb4
Oscar Castro <oscar.a.castro818@gmail.com>
2024-04-02 15:58:44 -0500

    fix add to cart btn with items not in stock

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -7,6 +7,7 @@ import {addToCart} from "../slices/cartSlice";
 import {toast} from "react-hot-toast";
 import {useDispatch} from "react-redux";
 import {useEffect, useRef} from "react";
+import CustomBtn from "./CustomBtn";
 
 
 const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWidth} ) => {
@@ -35,12 +36,14 @@ const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWi
     useEffect(() => {
         const handleButtonClick = (event) => {
             if (buttonRef?.current?.contains(event.target)) {
-                addToCartHandler();
+                if (product.countInStock !== 0) {
+                    addToCartHandler();
+                }
             }
         };
         window.addEventListener("click", handleButtonClick);
         return () => window.removeEventListener("click", handleButtonClick);
-    }, [addToCartHandler]);
+    }, [addToCartHandler, product.countInStock]);
 
 
     useEffect(() => {
@@ -97,14 +100,22 @@ const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWi
                                 <div className={"w-full flex flex-col sm:pt-2 justify-around"}>
                                     <span className={"sm:text-xl font-bold text-slate-500"}>${product.price}</span>
                                     <div ref={buttonRef} className={"my-2 w-10/12 h-full self-center"}>
-                                        <motion.button
-                                            whileTap={{scale: 0.9}}
-                                            className={`w-full relative rounded-full px-5 py-2 text-base overflow-hidden group bg-zinc-600 relative md:hover:bg-gradient-to-r md:hover:from-violet-600 md:hover:to-violet-500 text-white md:hover:ring-2 md:hover:ring-offset-0 md:hover:ring-violet-500 md:transition-all md:ease-out md:duration-300"}`}
-                                            type={"button"}
-                                        >
-                                            <span className={"md:absolute md:right-0 md:w-8 md:h-32 md:-mt-12 md:transition-all md:duration-500 md:transform md:translate-x-12 md:bg-white md:opacity-10 md:rotate-12 md:group-hover:-translate-x-40 md:ease"}/>
-                                            <span className={"relative ibmplex text-sm"}>Add To Cart</span>
-                                        </motion.button>
+                                        {
+                                            product.countInStock !== 0 ? (
+                                                <motion.button
+                                                    whileTap={{scale: 0.9}}
+                                                    className={`w-full relative rounded-full px-5 py-2 text-base overflow-hidden group bg-zinc-600 relative md:hover:bg-gradient-to-r md:hover:from-violet-600 md:hover:to-violet-500 text-white md:hover:ring-2 md:hover:ring-offset-0 md:hover:ring-violet-500 md:transition-all md:ease-out md:duration-300"}`}
+                                                    type={"button"}
+                                                >
+                                                    <span className={"md:absolute md:right-0 md:w-8 md:h-32 md:-mt-12 md:transition-all md:duration-500 md:transform md:translate-x-12 md:bg-white md:opacity-10 md:rotate-12 md:group-hover:-translate-x-40 md:ease"}/>
+                                                    <span className={"relative ibmplex text-sm"}>Add To Cart</span>
+                                                </motion.button>
+                                            ) : (
+                                                <CustomBtn isDisabled={true} customClass={"py-2 text-sm w-full"}>
+                                                    Out Of Stock
+                                                </CustomBtn>
+                                            )
+                                        }
                                     </div>
                                 </div>
                             )
