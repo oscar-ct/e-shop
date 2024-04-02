69e53d31e4a95535e4aff050fde5854d88d960a4
Oscar Castro <oscar.a.castro818@gmail.com>
2024-04-02 04:24:23 -0500

    implement add to cart btn, attempted to capture mouse and touch events, minor style update

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,13 +1,37 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import Rating from "./Rating";
 import {useState} from "react";
 import {motion} from "framer-motion";
 import Reveal from "./Reveal";
+import CustomBtn from "./CustomBtn";
+import {addToCart} from "../slices/cartSlice";
+import {toast} from "react-hot-toast";
+import {useDispatch} from "react-redux";
+
 
 const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWidth} ) => {
+    const [count, setCount] = useState(0);
     const [imgIndex, setImageIndex] = useState(0);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const quantity = 1;
+    const addToCartHandler = () => {
+        dispatch(addToCart({
+            ...product, quantity
+        }));
+        toast.success(() => {
+            return <Link to={"/cart"}>Added To Cart</Link>
+        });
+        // navigate("/cart");
+    };
+    const navigateHandler = () => {
+        if (count === 0) {
+            navigate(`/product/${product._id}`);
+        }
+    };
+
     return (
-        <Link to={`/product/${product._id}`} className={"w-6/12 sm:w-72 p-1 sm:p-3"}>
+        <div onClick={navigateHandler} className={"w-6/12 sm:w-72 p-1 sm:p-3 cursor-pointer"}>
             <Reveal
                 customParentClass={"h-full"}
                 customChildClass={"h-full"}
@@ -17,7 +41,7 @@ const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWi
                     transition={{ duration: 0.25 }}
                     whileHover={windowInnerWidth >= 768 ? { scale: 1.1} : {scale: 1}}
                     whileTap={windowInnerWidth >= 768 ? { scale: 0.9} : {scale: 1}}
-                    className={`${cardWidth && cardWidth} rounded-sm flex flex-col bg-white md:shadow-md h-full`}
+                    className={`${cardWidth && cardWidth} rounded-2xl border-[1px] border-black md:border-gray-300 flex flex-col bg-white md:shadow-md h-full`}
                 >
                     <div
                         onMouseEnter={() => (windowInnerWidth >= 500 && product.images.length > 1) && setImageIndex(product.images.length - (product.images.length - 1))}
@@ -35,7 +59,7 @@ const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWi
                             {/*<h2 className="text-concat text-sm font-semibold">{product.name}</h2>*/}
                         </div>
                         <div className={`w-full text-xs ${!smallSize ? "sm:text-base" : " flex justify-between items-center"}`}>
-                            <Rating rating={product.rating} text={`${product.numReviews} ${product.numReviews !== 1 ? "reviews" : "review"}`}/>
+                            <Rating rating={product.rating} text={`(${product.numReviews})`}/>
                             {
                                 smallSize && (
                                     <span className={"text-sm font-bold text-slate-500"}>
@@ -44,17 +68,23 @@ const ProductItem = ( {product, smallSize = false, cardWidth = "", windowInnerWi
                                 )
                             }
                         </div>
+
                         {
                             !smallSize && (
-                                <div className={"w-full flex sm:pt-2 justify-end"}>
+                                <div className={"w-full flex flex-col sm:pt-2 justify-around"}>
                                     <span className={"sm:text-xl font-bold text-slate-500"}>${product.price}</span>
+                                    <div className={"w-full flex justify-center py-2"} >
+                                        <CustomBtn onTouchStart={count + 1} onTouchEnd={count - 1} onMouseEnter={() => setCount(count + 1)} onMouseLeave={() => setCount(count - 1)} onClick={() => addToCartHandler()} customClass={"text-sm w-10/12 bg-zinc-600"}>
+                                            Add To Cart
+                                        </CustomBtn>
+                                    </div>
                                 </div>
                             )
                         }
                     </div>
                 </motion.div>
             </Reveal>
-        </Link>
+        </div>
 
     );
 };
