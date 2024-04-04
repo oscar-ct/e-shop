cbedaa32b6c2507e54b59da3178ce606de6b6172
Oscar Castro <oscar.a.castro818@gmail.com>
2024-04-04 17:21:58 -0500

    remove x axis padding to discount input

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -288,7 +288,7 @@ const CheckoutPage = () => {
                                                 ) : (
                                                     <>
                                                         <div
-                                                            className={"px-3 flex flex-col items-end lg:flex-col lg:items-end xl:items-center xl:flex-row xl:justify-between"}>
+                                                            className={"flex flex-col items-end lg:flex-col lg:items-end xl:items-center xl:flex-row xl:justify-between"}>
                                                             <input
                                                                 className={"bg-white w-full text-base px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-200 focus:outline-none focus:border-blue-400"}
                                                                 placeholder={"Have a discount code?"}
