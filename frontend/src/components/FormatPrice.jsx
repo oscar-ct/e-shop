bcffc0d92ae7561359034b3465d68bfeac273df1
Oscar Castro <oscar.a.castro818@gmail.com>
2024-04-03 19:56:09 -0500

    increase padding to match numheight

--- frontend/src/components/FormatPrice.jsx
+++ frontend/src/components/FormatPrice.jsx
@@ -7,7 +7,7 @@ const FormatPrice = ({children, price, fontSize}) => {
                     <span className={`${fontSize} font-bold`}>{price.toString().substring(0,  price.toString().indexOf("."))}</span>
                     <span className={"text-xs pt-1"}>{price.toString().substring(price.toString().indexOf(".")+1, price.toString().length).substring(0, 2)}</span>
                 </div>
-                <span className={"text-xs"}>{children}</span>
+                <span className={"text-xs pb-[2px]"}>{children}</span>
             </div>
         )
     } else {
@@ -17,7 +17,7 @@ const FormatPrice = ({children, price, fontSize}) => {
                     <span className={"text-xs pt-1"}>$</span>
                     <span className={`${fontSize} font-bold`}>{price}</span>
                 </div>
-                <span className={"text-sm"}>{children}</span>
+                <span className={"text-xs pb-[2px]"}>{children}</span>
             </div>
         )
     }
