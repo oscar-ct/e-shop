e09ae0541aa7ce66f40036056898bc2bdc49df42
Oscar Castro <oscar.a.castro818@gmail.com>
2024-04-05 13:15:42 -0500

    remove slice from products array

--- frontend/src/components/HomePageCategorySection.jsx
+++ frontend/src/components/HomePageCategorySection.jsx
@@ -1,14 +1,12 @@
 import CategoryItem from "./CategoryItem";
-import {FaChevronDown} from "react-icons/fa";
-import {useState} from "react";
 
 const HomePageCategorySection = ({productCategories, windowInnerWidth}) => {
 
-    const [categoryDropdownActive, setCategoryDropdownActive] = useState(false);
-
-    const rotateChevron = (action) => {
-        return action ? "open" : "closed";
-    };
+    // const [categoryDropdownActive, setCategoryDropdownActive] = useState(false);
+    //
+    // const rotateChevron = (action) => {
+    //     return action ? "open" : "closed";
+    // };
 
     return (
         <>
@@ -23,31 +21,29 @@ const HomePageCategorySection = ({productCategories, windowInnerWidth}) => {
 
                 <div className={"bg-white w-full flex flex-wrap justify-center md:border py-6"}>
                     {
-                        productCategories.slice(0, !categoryDropdownActive && windowInnerWidth < 768 ? 6 : !categoryDropdownActive && windowInnerWidth < 1024 && windowInnerWidth >= 768 ? 4 : !categoryDropdownActive && windowInnerWidth > 1024 && windowInnerWidth < 1280 ? 5 : !categoryDropdownActive && windowInnerWidth >= 1280 && windowInnerWidth < 1282 ? 6 :
-                            !categoryDropdownActive && windowInnerWidth >= 1282 && windowInnerWidth < 1536 ? 7 :
-                                !categoryDropdownActive && windowInnerWidth > 1536 ? 8 : 8).map(function (product, index) {
+                        productCategories.map(function (product, index) {
                             return <CategoryItem key={index} product={product} windowInnerWidth={windowInnerWidth}/>
                         })
                     }
                 </div>
-                <div className={"md:pt-3"}>
-                    <div className={"flex justify-end items-center px-2"}>
-                        <div>
-                            {/*<Link to={"/sort/latest/select/all"} className={"btn glass bg-neutral/70 text-white"}>*/}
-                            {/*    View All Categories*/}
-                            {/*</Link>*/}
-                            <button onClick={() => setCategoryDropdownActive(!categoryDropdownActive)} className={"flex items-center text-black hover:text-black/70"}>
-                                {categoryDropdownActive ? "Less Categories" : "More Categories"}
-                                <div className={`px-2 ${rotateChevron(categoryDropdownActive)}`}>
-                                    <FaChevronDown/>
-                                </div>
-                            </button>
-                        </div>
-                    </div>
+                {/*<div className={"md:pt-3"}>*/}
+                {/*    <div className={"flex justify-end items-center px-2"}>*/}
+                {/*        <div>*/}
+                {/*            /!*<Link to={"/sort/latest/select/all"} className={"btn glass bg-neutral/70 text-white"}>*!/*/}
+                {/*            /!*    View All Categories*!/*/}
+                {/*            /!*</Link>*!/*/}
+                {/*            <button onClick={() => setCategoryDropdownActive(!categoryDropdownActive)} className={"flex items-center text-black hover:text-black/70"}>*/}
+                {/*                {categoryDropdownActive ? "Less Categories" : "More Categories"}*/}
+                {/*                <div className={`px-2 ${rotateChevron(categoryDropdownActive)}`}>*/}
+                {/*                    <FaChevronDown/>*/}
+                {/*                </div>*/}
+                {/*            </button>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
                     {/*<div className={"h-10 footer bg-neutral border-b-[1px] border-grey-300"}>*/}
                     {/*<img onMouseOver={() => !charizardActive && setCharizardActive(true)} onClick={() => window.location.href = "https://oscar-ct.github.io/pok-mon/"} className={`w-14 ${!charizardActive && "opacity-50"}`} style={{position: "absolute", left: charizardPostion+"%", cursor: "pointer"}} src={charizard} alt={"charizard"}/>*/}
                     {/*</div>*/}
-                </div>
+            {/*    </div>*/}
             </div>
         </>
     );
