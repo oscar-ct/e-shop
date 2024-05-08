97ebfde8d6450cbfc58265b275519d51c3ab51fd
Oscar Castro <oscar.a.castro818@gmail.com>
2024-05-08 17:03:51 -0500

    adjust set y and x positions

--- frontend/src/components/ActiveLogo.jsx
+++ frontend/src/components/ActiveLogo.jsx
@@ -12,7 +12,7 @@ export const ActiveLogo = () => {
     return (
         <>
             <span className={`chat chat-start absolute top-[-150px] ${active ? "pl-10" : "pl-10"}`}>
-                <span className="chat-bubble">
+                <span className="chat-bubble text-sm">
                 {!active && count === 0 && "Hello there!"}
                 {active && count === 0 && "Catch me if you can!"}
                 {!active && count !== 0 && "Nice job!"}
@@ -20,17 +20,17 @@ export const ActiveLogo = () => {
             </span>
             <motion.div className="!opacity-0 !fixed !w-[300px] !h-[300px] !rounded-xl !top-0 !right-0 !left-0 !m-auto !bg-none" ref={constraintsRef} />
             <motion.div
-                animate={active && { x: x, y: y }}
+                animate={active ? { x: x, y: y } : {}}
                 transition={{
                     ease: "linear",
-                    duration: .15,
+                    duration: .3,
                     // type: "spring",
                     // stiffness: 100,
                     // repeat: 1
                 }}
                 onAnimationComplete={() => {
-                    setX(Math.ceil(Math.random() * 199) * (Math.round(Math.random()) ? 1 : -1));
-                    setY(Math.ceil(Math.random() * 99) * (Math.round(Math.random()) ? 1 : -1));
+                    setX(Math.ceil(Math.random() * 300) * (Math.round(Math.random()) ? 1 : -1));
+                    setY(Math.ceil(Math.random() * 150) * (Math.round(Math.random()) ? 1 : -1));
                 }}
                 dragElastic={1}
                 whileDrag={{ scale: 1.3 }}
@@ -40,7 +40,7 @@ export const ActiveLogo = () => {
                     !active && count === 0 && setActive(true);
                     // setCount(0);
                 }}
-                onClick={() => {
+                onClick={(e) => {
                     setActive(false);
                     setCount(count + 1);
                 }}
